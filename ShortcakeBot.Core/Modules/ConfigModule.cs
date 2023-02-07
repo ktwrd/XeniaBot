b032b992c268c5687ccc2126750a7d7a6ac905f9
kate <kate@dariox.club>
2023-02-07 20:38:16 +0800

    Create ConfigModule.cs

---
+++ ShortcakeBot.Core/Modules/ConfigModule.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShortcakeBot.Core.Modules
+{
+    [Discord.Interactions.Group("config", "Configure Shortcake")]
+    public class ConfigModule : InteractionModuleBase
+    {
+        [SlashCommand("ticket", "Configure the Ticket Module")]
+        public async Task TicketConfig(
+            [ChannelTypes(ChannelType.Category)] ICategoryChannel ticketCategory,
+            IRole roleThing)
+        {
+            await Context.Interaction.RespondAsync(".");
+        }
+    }
+}
