4c05a215ab5a2d198ced005855ae994f631a1470
kate! <kate@dariox.club>
2023-02-10 01:41:06 +0800

    Add Config file validation

--- SkidBot.Core/ConfigManager.cs
+++ SkidBot.Core/ConfigManager.cs
@@ -10,6 +10,7 @@ namespace SkidBot.Core
 {
     public class ConfigManager
     {
+        #region Read/Write
         public Config Read()
         {
             var config = new Config();
@@ -17,11 +18,21 @@ namespace SkidBot.Core
             {
                 var content = File.ReadAllText(Location);
                 config = JsonSerializer.Deserialize<Config>(content, Program.SerializerOptions);
+                var validationResponse = Validate(content);
+                if (validationResponse.Failure)
+                {
+                    Log.WriteLine($"Failed to validate config. Encountered {validationResponse.FailureCount} errors.");
+                    config = null;
+                }
+
                 if (config == null)
                 {
                     Log.Error($"Failed to parse config");
                     Log.Error(content);
                     Program.Quit(1);
+
+                    // This is only here to make VS2022 shut up
+                    return new Config();
                 }
             }
             else
@@ -37,6 +48,57 @@ namespace SkidBot.Core
             var content = JsonSerializer.Serialize(config, Program.SerializerOptions);
             File.WriteAllText(Location, content);
         }
+        #endregion
+
+        #region Config Validation
+        public static readonly string[] IgnoredValidationKeys = new string[]
+        {
+            "DeveloperMode",
+            "DeveloperMode_Server",
+            "UserWhitelistEnable",
+            "UserWhitelist",
+            "Prefix",
+            "GeneratorId"
+        };
+        public ConfigValidationResponse Validate(Dictionary<string, object> configDict)
+        {
+            Dictionary<string, object> defaultDict = JsonSerializer.Deserialize<Dictionary<string, object>>(
+                JsonSerializer.Serialize(new Config(), Program.SerializerOptions),
+                Program.SerializerOptions);
+
+            var unchangedNoIgnore = new List<string>();
+            var missing = new List<string>();
+
+            foreach (var defaultPair in defaultDict)
+            {
+                bool contains = configDict.ContainsKey(defaultPair.Key);
+                bool ignore = IgnoredValidationKeys.Contains(defaultPair.Key);
+                if (!contains)
+                {
+                    Log.Warn($"Missing key \"{defaultPair.Key}\"");
+                    missing.Add(defaultPair.Key);
+                    continue;
+                }
+
+                if (configDict[defaultPair.Key] == defaultPair.Value)
+                {
+                    unchangedNoIgnore.Add(defaultPair.Key);
+                    Log.Error($"Config Key \"{defaultPair.Key}\" has not been changed!");
+                }
+            }
+            var unchanged = unchangedNoIgnore.Where(v => !IgnoredValidationKeys.Contains(v));
+
+            return new ConfigValidationResponse()
+            {
+                UnchangedKeys = unchanged.ToArray(),
+                UnchangedKeysNoIgnore = unchangedNoIgnore.ToArray(),
+                MissingKeys = missing.ToArray()
+            };
+        }
+        public ConfigValidationResponse Validate(string fileContent) => Validate(JsonSerializer.Deserialize<Dictionary<string, object>>(fileContent, Program.SerializerOptions) ?? new Dictionary<string, object>());
+        public ConfigValidationResponse Validate(Config config) => Validate(JsonSerializer.Serialize(config, Program.SerializerOptions) ?? "{}");
+        #endregion
+        
         public string Location => Path.Combine(Directory.GetCurrentDirectory(), "config.json");
         public class Config
         {
@@ -49,6 +111,7 @@ namespace SkidBot.Core
             public ulong ErrorChannel = 0;
             public ulong ErrorGuild = 0;
             public string MongoDBServer = "";
+            public int GeneratorId = 0;
         }
     }
 }
