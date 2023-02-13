d93d9b19874fafbdca0af946e2181475df33ff90
kate! <kate@dariox.club>
2023-02-13 21:06:03 +0800

    Create BanSync Config Model/Controller

---
+++ SkidBot.Core/Controllers/BanSyncConfigController.cs
@@ -0,0 +1,64 @@
+﻿using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SkidBot.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Controllers
+{
+    public class BanSyncConfigController
+    {
+        private readonly IServiceProvider _services;
+        public BanSyncConfigController(IServiceProvider services)
+        {
+            _services = services;
+        }
+
+        public const string MongoCollectionName = "banSyncGuildConfig";
+        protected static IMongoCollection<T>? GetCollection<T>()
+        {
+            return Program.GetMongoDatabase()?.GetCollection<T>(MongoCollectionName);
+        }
+        protected static IMongoCollection<ConfigBanSyncModel>? GetCollection()
+            => GetCollection<ConfigBanSyncModel>();
+
+        public async Task<bool> Exists(ulong guildId)
+        {
+            var collection = GetCollection();
+            var result = await collection.FindAsync(GetFilter(guildId));
+            return result.Any();
+        }
+        public async Task<ConfigBanSyncModel> Get(ulong guildId)
+        {
+            var collection = GetCollection();
+            var result = await collection.FindAsync(GetFilter(guildId));
+
+            return result.FirstOrDefault();
+        }
+        public async Task Set(ConfigBanSyncModel data)
+        {
+            ulong guildId = data.GuildId;
+            var collection = GetCollection();
+            var filter = GetFilter(guildId);
+            if (await Exists(guildId))
+            {
+                await collection.ReplaceOneAsync(filter, data);
+            }
+            else
+            {
+                await collection.InsertOneAsync(data);
+            }
+        }
+        protected FilterDefinition<ConfigBanSyncModel> GetFilter(ulong guildId)
+        {
+            var filter = Builders<ConfigBanSyncModel>
+                .Filter
+                .Eq("GuildId", guildId);
+            return filter;
+        }
+    }
+}
---
+++ SkidBot.Core/Models/ConfigBanSyncModel.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Models
+{
+    public class ConfigBanSyncModel
+    {
+        [Browsable(false)]
+        public ObjectId _id { get; set; }
+        public ulong GuildId { get; set; }
+        public ulong LogChannel { get; set; }
+    }
+}
