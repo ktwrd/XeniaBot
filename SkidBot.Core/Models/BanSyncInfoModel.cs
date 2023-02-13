1086e4b2e86a67b350193643d8c6010b68659413
kate! <kate@dariox.club>
2023-02-13 21:06:11 +0800

    Create BanSync Info Model

---
+++ SkidBot.Core/Models/BanSyncInfoModel.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Models
+{
+    public class BanSyncInfoModel
+    {
+        [Browsable(false)]
+        public ObjectId _id { get; set; }
+        public ulong UserId { get; set; }
+        public ulong GuildId { get; set; }
+        public ulong BannerUserId { get; set; }
+        public double Timestamp { get; set; }
+        public string Reason { get; set; }
+        public BanSyncInfoModel()
+        {
+            Reason = "<unknown>";
+        }
+    }
+}
