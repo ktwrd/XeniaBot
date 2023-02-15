69430ae4266918f165dcb0cefb123e3a5eee25dc
kate <kate@dariox.club>
2023-02-15 08:31:33 +0800

    Add more fields and stuff

--- SkidBot.Core/Models/ConfigBanSyncModel.cs
+++ SkidBot.Core/Models/ConfigBanSyncModel.cs
@@ -15,5 +15,23 @@ namespace SkidBot.Core.Models
         public ulong GuildId { get; set; }
         public ulong LogChannel { get; set; }
         public bool Enable { get; set; }
+        public BanSyncGuildState State { get; set; }
+        public string Reason { get; set; }
+        public ConfigBanSyncModel()
+        {
+            GuildId = 0;
+            LogChannel = 0;
+            Enable = false;
+            State = BanSyncGuildState.Unknown;
+            Reason = "";
+        }
+    }
+    public enum BanSyncGuildState
+    {
+        Unknown = -1,
+        PendingRequest,
+        RequestDenied,
+        Blacklisted,
+        Active
     }
 }
