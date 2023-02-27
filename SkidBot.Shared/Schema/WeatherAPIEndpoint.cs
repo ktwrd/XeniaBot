88bae524b87d2417588d3ba7fa7b1239dc0eefaa
kate! <kate@dariox.club>
2023-02-27 16:28:31 +0800

    Add forecast endpoint

--- SkidBot.Shared/Schema/WeatherAPIEndpoint.cs
+++ SkidBot.Shared/Schema/WeatherAPIEndpoint.cs
@@ -21,9 +21,12 @@ namespace SkidBot.Shared.Schema
         };
         public static string BaseUrl => "http://api.weatherapi.com";
         private static string encode(string value) => WebUtility.UrlEncode(value);
+        private static string encodeBool(bool value) => value ? "yes" : "no";
         public static string Current(string key, string location, bool airQuality)
         => $"{BaseUrl}/v1/current.json?key={encode(key)}&q={encode(location)}&aqi=" + (airQuality ? "yes" : "no");
         public static string Search(string key, string query)
         => $"{BaseUrl}/v1/search.json?key={encode(key)}&q={encode(query)}";
+        public static string Forecast(string key, string location, int days, bool airQuality, bool alerts)
+        => $"{BaseUrl}/v1/forecast.json?key={encode(key)}&q={encode(location)}&days={days}&aqi={encodeBool(airQuality)}&alerts={encodeBool(alerts)}";
     }
 }
