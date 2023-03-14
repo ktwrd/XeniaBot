31a7c588cfd0f66ad9aee2b7fb724377adc5d22e
kate! <kate@dariox.club>
2023-03-14 21:10:35 +0800

    Create RoleMessageConfig Model&Controller

---
+++ SkidBot.Core/Models/RoleMessageConfigModel.cs
@@ -0,0 +1,27 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Models
+{
+    public class RoleMessageConfigModel
+    {
+        [Browsable(false)]
+        public ObjectId _id { get; set; }
+        public uint GuildId { get; set; }
+        public uint MessageId { get; set; }
+        /// <summary>
+        /// Key: <see cref="Discord.IEmote.Name"/>
+        /// Value: <see cref="RoleConfigModel.Uid"/>
+        /// </summary>
+        public Dictionary<string, string> ReactionRoleMap { get; set; }
+        public RoleMessageConfigModel()
+        {
+            ReactionRoleMap = new Dictionary<string, string>();
+        }
+    }
+}
