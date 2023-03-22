f4f95b0a360cc231161697728985f7c007f79435
kate <kate@dariox.club>
2023-03-22 11:24:10 +0800

    Create BaseModel.cs

---
+++ SkidBot.Core/Models/BaseModel.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Models
+{
+    public class BaseModel
+    {
+        [Browsable(false)]
+        public ObjectId _id { get; set; }
+    }
+}
