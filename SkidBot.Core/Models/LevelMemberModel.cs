e2a0055a7754c16252b1bd6ac84b3194cc414229
kate <kate@dariox.club>
2023-03-22 11:41:49 +0800

    Create LevelMemberModel.cs

---
+++ SkidBot.Core/Models/LevelMemberModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Models
+{
+    public class LevelMemberModel : BaseModel
+    {
+        public ulong UserId { get; set; }
+        public ulong GuildId { get; set; }
+        public ulong Xp { get; set; }
+
+        public LevelMemberModel()
+        {
+            UserId = 0;
+            GuildId = 0;
+            Xp = 0;
+        }
+    }
+}
