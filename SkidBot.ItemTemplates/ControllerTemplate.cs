eb6e42a036dbfd1ac3a1cb205ade3d59233b88e5
kate! <kate@dariox.club>
2023-03-22 11:25:08 +0800

    Create templates

---
+++ SkidBot.ItemTemplates/ControllerTemplate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkidBot.Shared;
+
+namespace $rootnamespace$
+{
+    [SkidControllerAttribute]
+    public class $safeitemrootname$ : BaseController
+    {
+        public $safeitemrootname$(IServiceProvider services)
+            : base (services)
+        {
+
+        }
+    }
+}
