18556bcaae3d2c80392fc9366cdf7fb7847da0e0
kate <kate@dariox.club>
2023-03-23 09:53:50 +0800

    Show level and progress percentage in Profile command

--- SkidBot.Core/Modules/XpModule.cs
+++ SkidBot.Core/Modules/XpModule.cs
@@ -2,6 +2,7 @@
 using Discord.Interactions;
 using Microsoft.Extensions.DependencyInjection;
 using SkidBot.Core.Controllers.BotAdditions;
+using SkidBot.Core.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,21 +23,26 @@ namespace SkidBot.Core.Modules
             }.Build(), ephemeral: true);
         }
 
-        [SlashCommand("profile", "See the amount of XP you have")]
+        [SlashCommand("profile", "See the amount of XP you have and what level you are")]
         public async Task Profile()
         {
             var controller = Program.Services.GetRequiredService<LevelSystemController>();
-            var data = await controller.Get(Context.User.Id, Context.Guild.Id);
+            var data = await controller.Get(Context.User.Id, Context.Guild.Id) ?? new Models.LevelMemberModel();
+            var metadata = LevelSystemHelper.Generate(data);
 
             var embed = new EmbedBuilder()
             {
-                Title = "XP Profile"
+                Footer = new EmbedFooterBuilder()
+                {
+                    Text = "XP: Profile"
+                },
+                Description = string.Join("\n", new string[]
+                {
+                $"**XP**: {data?.Xp ?? 0}",
+                $"**Progress**: {Math.Round(metadata.NextLevelProgress * 100, 3)}% ({metadata.UserXp - metadata.CurrentLevelStart}/{metadata.CurrentLevelEnd})",
+                $"**Level**: {metadata.UserLevel}"
+                })
             };
-
-            embed.WithDescription(string.Join("\n", new string[]
-            {
-                $"**XP**: {data?.Xp ?? 0}"
-            }));
 
             await Context.Interaction.RespondAsync(embed: embed.Build(), ephemeral: true);
         }
