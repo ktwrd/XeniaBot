227a6ac24eef7750e37f9dedd626b378b69c3c7c
kate <kate@dariox.club>
2023-03-24 13:38:40 +0800

    Update Question.cs

--- SkidBot.Shared/Schema/OpenTDB/Question.cs
+++ SkidBot.Shared/Schema/OpenTDB/Question.cs
@@ -1,8 +1,13 @@
-﻿using MongoDB.Bson.Serialization.Attributes;
+﻿using kate.shared.Helpers;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
@@ -10,6 +15,10 @@ namespace SkidBot.Shared.Schema.OpenTDB
 {
     public class OpenTDBQuestion
     {
+        [Browsable(false)]
+        [JsonIgnore]
+        public ObjectId _id { get; set; }
+
         [JsonPropertyName("category")]
         public string Category;
         [JsonPropertyName("type")]
@@ -23,7 +32,35 @@ namespace SkidBot.Shared.Schema.OpenTDB
         [JsonPropertyName("incorrect_answers")]
         public string[] IncorrectAnswers;
 
+        [JsonIgnore]
+        public string _QuestionHash => GetDataHash();
+        public string GetDataHash()
+        {
+            var options = new JsonSerializerOptions()
+            {
+                IgnoreReadOnlyFields = true,
+                IgnoreReadOnlyProperties = true,
+                IncludeFields = true
+            };
+            var json = JsonSerializer.Serialize(this, options);
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())
+            {
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(json));
+
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();
+                for (int i = 0; i < bytes.Length; i++)
+                {
+                    builder.Append(bytes[i].ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
         [BsonIgnore]
+        [JsonIgnore]
         public OpenTDBDifficulty Difficulty
         {
             get
@@ -42,6 +79,7 @@ namespace SkidBot.Shared.Schema.OpenTDB
         }
 
         [BsonIgnore]
+        [JsonIgnore]
         public OpenTDBType Type
         {
             get
@@ -58,6 +96,7 @@ namespace SkidBot.Shared.Schema.OpenTDB
         }
 
         [BsonIgnore]
+        [JsonIgnore]
         public string[] Answers
         {
             get
