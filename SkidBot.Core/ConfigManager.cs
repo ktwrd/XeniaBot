0d5aee71fc8ea17dc8e97c9848b23d99bd494cb7
kate <kate@dariox.club>
2023-03-24 09:52:45 +0800

    Add Google Cloud Config for Translate

--- SkidBot.Core/ConfigManager.cs
+++ SkidBot.Core/ConfigManager.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace SkidBot.Core
@@ -116,6 +117,43 @@ namespace SkidBot.Core
             public ulong BanSync_GlobalLogChannel = 0;
             public ulong BanSync_RequestChannel = 0;
             public string WeatherAPI_Key = "";
+            public GoogleCloudKey GCSKey_Translate = new GoogleCloudKey();
+        }
+        public class GoogleCloudKey
+        {
+            [JsonPropertyName("type")]
+            public string Type;
+            [JsonPropertyName("project_id")]
+            public string ProjectId;
+            [JsonPropertyName("private_key_id")]
+            public string PrivateKeyId;
+            [JsonPropertyName("private_key")]
+            public string PrivateKey;
+            [JsonPropertyName("client_email")]
+            public string ClientEmail;
+            [JsonPropertyName("client_id")]
+            public string ClientId;
+            [JsonPropertyName("auth_uri")]
+            public string AuthUri;
+            [JsonPropertyName("token_uri")]
+            public string TokenUri;
+            [JsonPropertyName("auth_provider_x509_cert_url")]
+            public string AuthProviderCertUrl;
+            [JsonPropertyName("client_x509_cert_url")]
+            public string ClientCertUrl;
+            public GoogleCloudKey()
+            {
+                Type = "";
+                ProjectId = "";
+                PrivateKeyId = "";
+                PrivateKey = "";
+                ClientEmail = "";
+                ClientId = "";
+                AuthUri = "";
+                TokenUri = "";
+                AuthProviderCertUrl = "";
+                ClientCertUrl = "";
+            }
         }
     }
 }
