3ff6af50aba4c95b957fbcbec9588269219e10dc
kate <kate@dariox.club>
2023-03-24 13:38:38 +0800

    Add function to get questions

--- SkidBot.Core/Controllers/Wrappers/OpenTDBController.cs
+++ SkidBot.Core/Controllers/Wrappers/OpenTDBController.cs
@@ -1,9 +1,11 @@
 ﻿using SkidBot.Shared;
+using SkidBot.Shared.Schema.OpenTDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 
 namespace SkidBot.Core.Controllers.Wrappers
@@ -11,11 +13,33 @@ namespace SkidBot.Core.Controllers.Wrappers
     [SkidController]
     public class OpenTDBController : BaseController
     {
-        private HttpClient httpClient;
+        private HttpClient _httpClient;
         public OpenTDBController(IServiceProvider services)
             : base(services)
         {
-            httpClient = new HttpClient();
+            _httpClient = new HttpClient();
+        }
+
+        public const string Endpoint = "https://opentdb.com";
+        public async Task<OpenTDBResponse> FetchQuestions(int amount = 10)
+        {
+            var url = $"{Endpoint}/api.php?amount={amount}";
+            var response = await _httpClient.GetAsync(url);
+            var stringContent = response.Content.ReadAsStringAsync().Result;
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+            {
+                string errorContent = $"Failed to fetch questions from \"{url}\" (code: {response.StatusCode})\n========String Content========\n{stringContent}";
+                Log.Error(errorContent);
+                throw new Exception(errorContent);
+                return null;
+            }
+
+            var deser = JsonSerializer.Deserialize<OpenTDBResponse>(stringContent, Program.SerializerOptions);
+            if (deser == null)
+            {
+                throw new Exception("Failed to deserialize");
+            }
+            return deser;
         }
     }
 }
