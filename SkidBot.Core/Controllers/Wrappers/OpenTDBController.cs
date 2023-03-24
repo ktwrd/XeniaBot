8a77c228794e49d406d545cd32ec21ee879dd7c8
kate <kate@dariox.club>
2023-03-24 11:18:05 +0800

    Create OpenTDB Controller

---
+++ SkidBot.Core/Controllers/Wrappers/OpenTDBController.cs
@@ -0,0 +1,21 @@
+﻿using SkidBot.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Core.Controllers.Wrappers
+{
+    [SkidController]
+    public class OpenTDBController : BaseController
+    {
+        private HttpClient httpClient;
+        public OpenTDBController(IServiceProvider services)
+            : base(services)
+        {
+            httpClient = new HttpClient();
+        }
+    }
+}
