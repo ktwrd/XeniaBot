6692e75765fcffd4c26a43eee6ca6e4342d00582
kate <kate@dariox.club>
2023-03-24 11:17:08 +0800

    Add response schema for OpenTDB

---
+++ SkidBot.Shared/Schema/OpenTDB/Question.cs
@@ -0,0 +1,88 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SkidBot.Shared.Schema.OpenTDB
+{
+    public class OpenTDBQuestion
+    {
+        [JsonPropertyName("category")]
+        public string Category;
+        [JsonPropertyName("type")]
+        public string TypeValue;
+        [JsonPropertyName("difficulty")]
+        public string DifficultyValue;
+        [JsonPropertyName("question")]
+        public string Question;
+        [JsonPropertyName("correct_answer")]
+        public string CorrectAnswer;
+        [JsonPropertyName("incorrect_answers")]
+        public string[] IncorrectAnswers;
+
+        [BsonIgnore]
+        public OpenTDBDifficulty Difficulty
+        {
+            get
+            {
+                switch (DifficultyValue)
+                {
+                    case "easy":
+                        return OpenTDBDifficulty.Easy;
+                    case "medium":
+                        return OpenTDBDifficulty.Medium;
+                    case "hard":
+                        return OpenTDBDifficulty.Hard;
+                }
+                return OpenTDBDifficulty.Unknown;
+            }
+        }
+
+        [BsonIgnore]
+        public OpenTDBType Type
+        {
+            get
+            {
+                switch (TypeValue)
+                {
+                    case "multiple":
+                        return OpenTDBType.MultipleChoice;
+                    case "boolean":
+                        return OpenTDBType.Boolean;
+                }
+                return OpenTDBType.Unknown;
+            }
+        }
+
+        [BsonIgnore]
+        public string[] Answers
+        {
+            get
+            {
+                var rnd = new Random();
+                var us = new string[]
+                {
+                    CorrectAnswer
+                }.Concat(IncorrectAnswers).ToArray();
+                rnd.Shuffle(us);
+                return us;
+            }
+        }
+    }
+    public enum OpenTDBDifficulty
+    {
+        Unknown = -1,
+        Easy,
+        Medium,
+        Hard
+    }
+    public enum OpenTDBType
+    {
+        Unknown = -1,
+        MultipleChoice,
+        Boolean
+    }
+}
---
+++ SkidBot.Shared/Schema/OpenTDB/Response.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SkidBot.Shared.Schema.OpenTDB
+{
+    public class OpenTDBResponse
+    {
+        [JsonPropertyName("response_code")]
+        public int ResponseCodeValue = -1;
+        public OpenTDBResponseCode ResponseCode => (OpenTDBResponseCode)ResponseCodeValue;
+        [JsonPropertyName("results")]
+        public OpenTDBQuestion[] Results = Array.Empty<OpenTDBQuestion>();
+    }
+    public enum OpenTDBResponseCode
+    {
+        Unknown = -1,
+        Success = 0,
+        NoResults = 1,
+        InvalidParameter = 2,
+        TokenNotFound = 3,
+        TokenEmpty = 4
+    }
+}
