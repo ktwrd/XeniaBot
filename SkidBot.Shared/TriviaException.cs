5be46243f6bbafe093e060038863f42fcc65ca77
kate <kate@dariox.club>
2023-04-06 16:22:12 +0800

    Create TriviaException.cs

---
+++ SkidBot.Shared/TriviaException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkidBot.Shared
+{
+    public class TriviaException : Exception
+    {
+        public TriviaException()
+            : base()
+        { }
+
+        public TriviaException(string? message)
+            : base(message)
+        { }
+        public TriviaException(string? message, Exception? innerException)
+            : base(message, innerException)
+        { }
+    }
+}
