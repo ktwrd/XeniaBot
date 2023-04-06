9061c21e226c751b4227bcdc4d5a5c245bebb36e
kate <kate@dariox.club>
2023-04-06 16:31:04 +0800

    Move CreateTriviaSession to TriviaSessionController

--- SkidBot.Core/Controllers/BotAdditions/TriviaSessionController.cs
+++ SkidBot.Core/Controllers/BotAdditions/TriviaSessionController.cs
@@ -1,6 +1,9 @@
-﻿using MongoDB.Driver;
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using SkidBot.Core.Controllers.Wrappers;
 using SkidBot.Core.Models;
 using SkidBot.Shared;
+using SkidBot.Shared.Schema.OpenTDB;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +15,67 @@ namespace SkidBot.Core.Controllers.BotAdditions
     [SkidController]
     public class TriviaSessionController : BaseController
     {
+        private OpenTDBController _tdb;
         public TriviaSessionController(IServiceProvider services)
             : base(services)
         {
-            
+            _tdb = services.GetRequiredService<OpenTDBController>();
+        }
+
+
+        /// <summary>
+        /// Safely create a trivia session.
+        /// </summary>
+        public async Task<TriviaSessionModel> CreateTriviaSession(ulong guildId, ulong channelId, int questionCount = 10, string? category = null)
+        {
+            if (questionCount < 1)
+            {
+                throw new ArgumentException($"Parameter questionCount must be greater than 0 (got {questionCount})");
+            }
+
+            var data = await Get(guildId, channelId);
+            if (data != null)
+            {
+                if (data.QuestionStack.Length <= data.QuestionsCompleted)
+                {
+                    throw new TriviaException("Session is already in progress");
+                }
+            }
+
+            // Initialize new data and populate question stack.
+            data = new TriviaSessionModel(guildId, channelId, questionCount);
+
+            // Populate question stack
+            OpenTDBResponse openTDBResponse;
+            try
+            {
+                openTDBResponse = await _tdb.FetchQuestions(questionCount, category);
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failed to generate question stack.\n{ex}");
+                throw new TriviaException("Failed to generate question stack.", ex);
+            }
+
+            // Validate response results
+            if (openTDBResponse.Results.Length < 1 || openTDBResponse.Results.Length != questionCount)
+            {
+                Log.Error($"Invalid amount of questions recieved (Expected {questionCount}, got {openTDBResponse.Results.Length}");
+                throw new TriviaException($"Invalid amount of questions recieved from server. Expected {questionCount}, got {openTDBResponse.Results.Length}");
+            }
+
+            // Cast OpenTDBQuestion to TriviaSessionQuestionModel
+            var questionStackList = new List<TriviaSessionQuestionModel>();
+            foreach (var i in openTDBResponse.Results)
+            {
+                questionStackList.Add(TriviaSessionQuestionModel.FromQuestion(i));
+            }
+            data.QuestionStack = questionStackList.ToArray();
+
+            await Set(data);
+
+            // Should never be null since we just pushed it to the database above.
+            return await Get(data);
         }
 
         public const string MongoCollectionName = "triviaSession";
