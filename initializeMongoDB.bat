96de1685a3635651237b0df880a472a8a5e1c20c
kate! <kate@dariox.club>
2023-04-11 13:55:23 +0800

    Persist data to folder mongoData

--- initializeMongoDB.bat
+++ initializeMongoDB.bat
@@ -2,4 +2,4 @@
 echo Creating database with name of "mongodb-shortcake"
 echo Username: user
 echo Password: password
-docker run -p 27020:27017 --name mongodb-shortcake -d -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=password mongo
+docker run -v mongoData:/data/db -p 27020:27017 --name mongodb-shortcake -d -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=password mongo
