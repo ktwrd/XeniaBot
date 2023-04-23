0717661768d2573510f1dd25d8e5c7f82162be7d
kate! <kate@dariox.club>
2023-04-23 12:04:33 +0800

    Add invite command

--- SkidBot.Core/Modules/MiscModule.cs
+++ SkidBot.Core/Modules/MiscModule.cs
@@ -77,5 +77,20 @@ namespace SkidBot.Core.Modules
 
             await Context.Interaction.RespondAsync("Done!", ephemeral: true);
         }
+
+        [SlashCommand("invite", "Get invite link for SkidBot")]
+        public async Task Invite()
+        {
+            var config = Program.Services.GetRequiredService<SkidConfig>();
+            var inviteLink =
+                $"https://discord.com/oauth2/authorize?client_id={config.Invite_ClientId}&scope=bot&permissions={config.Invite_Permissions}";
+            await Context.Interaction.RespondAsync(embed: DiscordHelper.BaseEmbed()
+                .WithUrl(inviteLink)
+                .WithTitle("Invite SkidBot")
+                .WithColor(Color.DarkGreen)
+                .WithDescription(
+                    "To invite SkidBot to your own server, click on the \"Invite SkidBot\" link and it should take you to Discord's Website to invite SkidBot to any of your servers!")
+                .Build());
+        }
     }
 }
