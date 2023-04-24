57c3d17298538100a1885f0f9e9ef9f13293ad59
kate! <kate@dariox.club>
2023-04-24 15:41:45 +0800

    Create BigBrotherController

---
+++ SkidBot.Core/Controllers/Wrappers/BigBrotherController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using SkidBot.Shared;
+
+namespace SkidBot.Core.Controllers.Wrappers;
+
+[SkidController]
+public class BigBrotherController : BaseController
+{
+    public BigBrotherController(IServiceProvider services)
+        : base(services)
+    {
+        
+    }
+}
