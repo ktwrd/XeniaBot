5ee50682e5019ebc8a97d9e84794ec0edd2681a4
kate! <kate@dariox.club>
2023-04-24 19:36:51 +0800

    Create Delegates.cs

---
+++ SkidBot.Core/Controllers/Wrappers/BigBrother/Delegates.cs
@@ -0,0 +1,12 @@
+﻿namespace SkidBot.Core.Controllers.Wrappers.BigBrother;
+
+public enum MessageChangeType
+{
+    Create,
+    Delete,
+    Update
+}
+public delegate void MessageDiffDelegate(
+    MessageChangeType type,
+    BB_MessageModel current,
+    BB_MessageModel? previous);
