288ef4fb1b4dfbb85221845e5af0166a40342ac4
kate <kate@dariox.club>
2023-04-26 16:26:56 +0800

    Reorder calculation

--- SkidBot.Core/Helpers/BigBrotherHelper.cs
+++ SkidBot.Core/Helpers/BigBrotherHelper.cs
@@ -23,22 +23,22 @@ public static class BigBrotherHelper
     {
         if (channel is SocketCategoryChannel)
             return BB_ChannelType.Category;
+        else if (channel is SocketGroupChannel)
+            return BB_ChannelType.Group;
         else if (channel is SocketDMChannel)
             return BB_ChannelType.DM;
         else if (channel is SocketForumChannel)
             return BB_ChannelType.Forum;
-        else if (channel is SocketGroupChannel)
-            return BB_ChannelType.Group;
         else if (channel is SocketNewsChannel)
             return BB_ChannelType.News;
         else if (channel is SocketStageChannel)
             return BB_ChannelType.Stage;
-        else if (channel is SocketTextChannel)
-            return BB_ChannelType.Text;
         else if (channel is SocketThreadChannel)
             return BB_ChannelType.Thread;
         else if (channel is SocketVoiceChannel)
             return BB_ChannelType.Voice;
+        else if (channel is SocketTextChannel)
+            return BB_ChannelType.Text;
         else
             return BB_ChannelType.Unknown;
     }
