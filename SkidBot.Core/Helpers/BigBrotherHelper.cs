c21d2a2b0077da961d6bdbc57e3dd58411d5e13c
kate <kate@dariox.club>
2023-04-26 15:51:58 +0800

    Add method GetChannelType

--- SkidBot.Core/Helpers/BigBrotherHelper.cs
+++ SkidBot.Core/Helpers/BigBrotherHelper.cs
@@ -1,4 +1,6 @@
 ﻿using System.Text.Json;
+using Discord.WebSocket;
+using SkidBot.Core.Controllers.Wrappers.BigBrother;
 
 namespace SkidBot.Core.Helpers;
 
@@ -15,5 +17,35 @@ public static class BigBrotherHelper
         var text = JsonSerializer.Serialize(input, options);
         var output = JsonSerializer.Deserialize<TH>(text, options);
         return output;
+    }
+
+    public static BB_ChannelType GetChannelType<T>(T channel) where T : SocketChannel
+    {
+        if (channel is SocketCategoryChannel)
+            return BB_ChannelType.Category;
+        else if (channel is SocketDMChannel)
+            return BB_ChannelType.DM;
+        else if (channel is SocketForumChannel)
+            return BB_ChannelType.Forum;
+        else if (channel is SocketGroupChannel)
+            return BB_ChannelType.Group;
+        else if (channel is SocketNewsChannel)
+            return BB_ChannelType.News;
+        else if (channel is SocketStageChannel)
+            return BB_ChannelType.Stage;
+        else if (channel is SocketTextChannel)
+            return BB_ChannelType.Text;
+        else if (channel is SocketThreadChannel)
+            return BB_ChannelType.Thread;
+        else if (channel is SocketVoiceChannel)
+            return BB_ChannelType.Voice;
+        else
+            return BB_ChannelType.Unknown;
+    }
+
+    public static string? GetChannelName(SocketChannel channel)
+    {
+        var channelType = GetChannelType(channel);
+        
     }
 }
