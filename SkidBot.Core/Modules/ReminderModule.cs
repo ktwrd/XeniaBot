14cae07a066a6c15f02976071d13cb509f62de7a
kate! <kate@dariox.club>
2023-05-09 18:53:07 +0800

    Add Reminder module

---
+++ SkidBot.Core/Modules/ReminderModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using Microsoft.Extensions.DependencyInjection;
+using SkidBot.Core.Controllers.BotAdditions;
+using SkidBot.Core.Helpers;
+
+namespace SkidBot.Core.Modules;
+
+public class ReminderModule : InteractionModuleBase
+{
+    [SlashCommand("remind", "Create a reminder")]
+    public async Task CreateReminder(string when, string? note = null)
+    {
+        var timeSpan = TimeHelper.ParseFromString(when);
+        var timestamp = DateTimeOffset.UtcNow.Add(timeSpan).ToUnixTimeSeconds();
+        var controller = Program.Services.GetRequiredService<ReminderController>();
+        await controller.CreateReminderTask(
+            timestamp,
+            Context.User.Id,
+            Context.Channel.Id,
+            Context.Guild.Id,
+            note);
+
+        var embed = DiscordHelper.BaseEmbed()
+            .WithDescription($"Reminder will be sent <t:{timestamp}:R>")
+            .WithColor(Color.Green);
+        await Context.Interaction.RespondAsync(embed: embed.Build());
+    }
+}
