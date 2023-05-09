6a74377227872c5e5915e609580a6a89141f4ff6
kate! <kate@dariox.club>
2023-05-09 19:29:28 +0800

    Add help command for Remind module

---
+++ SkidBot.Core/Modules/HelpModule.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using Discord.Interactions;
+using SkidBot.Core.Helpers;
+
+namespace SkidBot.Core.Modules;
+
+[Discord.Interactions.Group("help", "Usage help for commands and modules")]
+public class HelpModule : InteractionModuleBase
+{
+    [SlashCommand("remind", "Usage for the reminder command")]
+    public async Task Remind()
+    {
+        var embed = DiscordHelper.BaseEmbed()
+            .WithTitle("Remind Module Help")
+            .WithDescription(string.Join("\n\n", new string[]
+            {
+                "To use the reminder system, you can call it with the `/remind` command.",
+                "For the `when` parameter it supports unordered day/hour/minute/second definition. So if you want to set a reminder for 3 days and 40min you can use the following command",
+                "`/remind 3d 40m`"
+            }));
+        await Context.Interaction.RespondAsync(embed: embed.Build());
+    }
+}
