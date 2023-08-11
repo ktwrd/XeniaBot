3680dda30d17f25de9c1049151cba5fb8d8b52be
kate <kate@dariox.club>
2023-08-11 19:02:48 +0800

    Create EconomyController.cs

---
+++ XeniaBot.Core/Controllers/BotAdditions/Economy/EconomyController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Discord.WebSocket;
+using Microsoft.Extensions.DependencyInjection;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Core.Controllers.BotAdditions;
+
+[BotController]
+public class EconomyController : BaseController
+{
+    private readonly DiscordSocketClient _client;
+    private readonly DiscordController _discord;
+    public EconomyController(IServiceProvider services)
+        : base(services)
+    {
+        _client = services.GetRequiredService<DiscordSocketClient>();
+        _discord = services.GetRequiredService<DiscordController>();
+    }
+}
