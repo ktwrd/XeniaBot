272d80ee9c0c43d9220afab352ebee86675fbc08
kate <kate@dariox.club>
2023-08-11 19:08:32 +0800

    Create DiceModule.cs

---
+++ XeniaBot.Core/Modules/DiceModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+
+namespace XeniaBot.Core.Modules;
+
+public class DiceModule : InteractionModuleBase
+{
+    [SlashCommand("dice", "Throw a dice. Defaults to a 6-sided dice")]
+    public async Task Dice(int max, int min = 1)
+    {
+        var result = Program.Random.Next(min, max);
+        var embed = new EmbedBuilder()
+            .WithTitle("Dice")
+            .WithDescription($"Resulted in `{result}` (between {min} and {max})")
+            .WithColor(Color.Blue);
+        await RespondAsync(embed: embed.Build());
+    }
+}
