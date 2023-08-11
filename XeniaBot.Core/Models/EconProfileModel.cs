bcfb6badaccd453572f59e7552ac531d2d245e48
kate <kate@dariox.club>
2023-08-11 19:02:52 +0800

    Create EconProfileModel.cs

---
+++ XeniaBot.Core/Models/EconProfileModel.cs
@@ -0,0 +1,9 @@
+﻿namespace XeniaBot.Core.Models;
+
+public class EconProfileModel : BaseModel
+{
+    public ulong UserId { get; set; }
+    public ulong GuildId { get; set; }
+    public long Coins { get; set; }
+    public long LastDailyTimestamp { get; set; }
+}
