e163d941bc1eea43597ba66e10c89e47af0c9a98
kate <kate@dariox.club>
2023-08-11 18:27:26 +0800

    Add Xp Server Leaderboard

--- XeniaBot.Core/Modules/XpModule.cs
+++ XeniaBot.Core/Modules/XpModule.cs
@@ -38,5 +38,71 @@ namespace XeniaBot.Core.Modules
 
             await Context.Interaction.RespondAsync(embed: embed.Build(), ephemeral: true);
         }
+
+        public async Task<EmbedBuilder> GenerateServerLeaderboard(ulong guildId)
+        {
+            var embed = new EmbedBuilder()
+                .WithTitle("Xp System - Guild Leaderboard");
+            
+            var controller = Program.Services.GetRequiredService<LevelSystemController>();
+            if (controller == null)
+            {
+                embed.WithDescription($"Could not fetch LevelSystemController");
+                embed.WithColor(Color.Red);
+                return embed;
+            }
+
+            var data = await controller.GetGuild(guildId);
+            var resultLines = new List<string>()
+            {
+                $"Top 5 of {data.Length} records."
+            };
+
+            var sorted = data.OrderByDescending(v => v.Xp).ToArray();
+            var length = Math.Min(5, sorted.Length);
+            string[] rankText = new string[]
+            {
+                ":1st_place_medal:",
+                ":2nd_place_medal:",
+                ":3rd_place_medal:",
+                ":four:",
+                ":five:"
+            };
+            for (int i = 0; i < length; i++)
+            {
+                var item = sorted[i];
+                var details = LevelSystemHelper.Generate(item.Xp);
+                resultLines.Add(string.Join(' ', new string[]
+                {
+                    rankText[i],
+                    $"`{item.Xp}xp, level {details.UserLevel}`",
+                    $"<@{item.UserId}>"
+                }));
+            }
+
+            embed.Description = string.Join("\n", resultLines);
+            return embed;
+        }
+        
+
+        [SlashCommand("leaderboard", "View this guild's XP Leaderboard")]
+        public async Task Leaderboard()
+        {
+            try
+            {
+                var embed = await GenerateServerLeaderboard(Context.Guild.Id);
+                await Context.Interaction.RespondAsync(embed: embed.Build(), ephemeral:true);
+            }
+            catch (Exception ex)
+            {
+                var embed = new EmbedBuilder()
+                    .WithTitle("Xp System - Error")
+                    .WithDescription($"Failed to generate server leaderboard\n```\n{ex.Message}\n```")
+                    .WithColor(Color.Red);
+                await Context.Interaction.RespondAsync(embed: embed.Build(), ephemeral: true);
+                await DiscordHelper.ReportError(ex, Context);
+                return;
+            }
+        }
     }
 }
