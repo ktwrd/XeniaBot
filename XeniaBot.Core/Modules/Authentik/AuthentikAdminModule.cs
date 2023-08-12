bc3743b4a63500fbcf46d7904e7eeef367888d49
kate <kate@dariox.club>
2023-08-12 15:30:02 +0800

    [Authentik] Create command group

---
+++ XeniaBot.Core/Modules/Authentik/AuthentikAdminModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Interactions;
+using XeniaBot.Core.Helpers;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Core.Modules;
+
+
+[Group("auth", "Authentik Administration")]
+[RequireOwner]
+public partial class AuthentikAdminModule : InteractionModuleBase
+{
+}
