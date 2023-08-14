52e02cc811fa073dbfc2f080e473d030f45a8419
kate <kate@dariox.club>
2023-08-14 20:06:43 +0800

    Add method FromGuild

--- XeniaBot.WebPanel/Models/StrippedChannel.cs
+++ XeniaBot.WebPanel/Models/StrippedChannel.cs
@@ -6,4 +6,16 @@ public class StrippedChannel
 {
     public ulong Id { get; set; }
     public string Name { get; set; }
+
+    public static IEnumerable<StrippedChannel> FromGuild(SocketGuild guild)
+    {
+        return guild.Channels.Select((v) =>
+        {
+            return new StrippedChannel()
+            {
+                Id = v.Id,
+                Name = v.Name
+            };
+        });
+    }
 }
