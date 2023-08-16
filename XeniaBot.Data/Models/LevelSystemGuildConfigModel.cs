6d0e8c21b031f979c6290e524237e64312b80fc0
kate <kate@dariox.club>
2023-08-16 17:37:38 +0800

    Add field Enable

--- XeniaBot.Data/Models/LevelSystemGuildConfigModel.cs
+++ XeniaBot.Data/Models/LevelSystemGuildConfigModel.cs
@@ -7,10 +7,12 @@ public class LevelSystemGuildConfigModel : BaseModel
     public ulong GuildId { get; set; }
     public ulong? LevelUpChannel { get; set; }
     public bool ShowLeveUpMessage { get; set; }
+    public bool Enable { get; set; }
 
     public LevelSystemGuildConfigModel()
     {
         LevelUpChannel = null;
+        Enable = true;
         ShowLeveUpMessage = true;
     }
 }
