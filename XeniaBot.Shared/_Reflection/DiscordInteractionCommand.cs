b7763d4f04597a1f41a28ddd57aa7cb16a327c56
kate <kate@dariox.club>
2023-08-16 23:36:36 +0800

    Add support to send available commands to DiscordBotList.com

---
+++ XeniaBot.Shared/_Reflection/DiscordInteractionCommand.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using Discord;
+using Discord.Rest;
+using Newtonsoft.Json;
+
+namespace XeniaBot.Shared;
+
+public class DiscordApplicationCommand
+  {
+    [JsonPropertyName("version")]
+    public ulong Version { get; set; }
+    public DiscordApplicationCommand Cast(RestApplicationCommand cmd, Dictionary<string, string[]> blacklistSubCommands)
+    {
+      this.Id = cmd.Id;
+      this.Version = ulong.Parse(new Random().NextInt64(0, long.MaxValue).ToString());
+      this.Type = cmd.Type;
+      this.ApplicationId = cmd.ApplicationId;
+      this.Name = cmd.Name;
+      this.Description = cmd.Description;
+      this.DefaultPermissions = cmd.IsDefaultPermission;
+      this.GuildId = null;
+
+      this.Options = null;
+      if (cmd.Options?.Count > 0)
+      {
+        this.Options = cmd.Options.Select(v => new DiscordCommandOption().Cast(v)).ToArray();
+        string[] blacklistBansyncName = new string[]
+        {
+          "enableguild", "setguildstate"
+        };
+        if (blacklistSubCommands.TryGetValue(this.Name, out var blacklistArr))
+          this.Options = this.Options.Where(v => !blacklistArr.Contains(v.Name)).ToArray();
+      }
+
+      this.DefaultMemberPermission = new GuildPermission?((GuildPermission)cmd.DefaultMemberPermissions.RawValue);
+      this.DmPermission = cmd.IsEnabledInDm;
+      this.Nsfw = cmd.IsNsfw;
+      return this;
+    }
+
+    [JsonPropertyName("id")]
+    public ulong Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public ApplicationCommandType Type { get; set; } = ApplicationCommandType.Slash;
+
+    [JsonPropertyName("application_id")]
+    public ulong ApplicationId { get; set; }
+
+    [JsonPropertyName("guild_id")]
+    public ulong? GuildId { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("options")]
+    public DiscordCommandOption[]? Options { get; set; }
+
+    [JsonPropertyName("default_permission")]
+    public bool? DefaultPermissions { get; set; }
+
+    [JsonPropertyName("dm_permission")]
+    public bool? DmPermission { get; set; }
+
+    [JsonPropertyName("default_member_permissions")]
+    public GuildPermission? DefaultMemberPermission { get; set; }
+
+    [JsonPropertyName("nsfw")]
+    public bool? Nsfw { get; set; }
+  }
+  
+  public class DiscordCommandOption
+  {
+    [JsonPropertyName("type")]
+    public ApplicationCommandOptionType Type { get; set; }
+
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("description")]
+    public string Description { get; set; }
+
+    [JsonPropertyName("default")]
+    public bool? Default { get; set; }
+
+    [JsonPropertyName("required")]
+    public bool? Required { get; set; }
+
+    [JsonPropertyName("choices")]
+    public DiscordCommandOptionChoice[]? Choices { get; set; }
+
+    [JsonPropertyName("options")]
+    public DiscordCommandOption[]? Options { get; set; }
+
+    [JsonPropertyName("autocomplete")]
+    public bool? Autocomplete { get; set; }
+
+    [JsonPropertyName("min_value")]
+    public double? MinValue { get; set; }
+
+    [JsonPropertyName("max_value")]
+    public double? MaxValue { get; set; }
+
+    [JsonPropertyName("channel_types")]
+    public ChannelType[]? ChannelTypes { get; set; }
+
+    [JsonPropertyName("min_length")]
+    public int? MinLength { get; set; }
+
+    [JsonPropertyName("max_length")]
+    public int? MaxLength { get; set; }
+
+    public DiscordCommandOption Cast(RestApplicationCommandOption opt)
+    {
+      this.Type = opt.Type;
+      this.Name = opt.Name;
+      this.Description = opt.Description;
+      
+      this.Default = opt.IsDefault;
+      
+      this.Required = opt.IsRequired;
+      
+      this.MinValue = opt.MinValue;
+
+      this.MaxValue = opt.MaxValue;
+
+      this.Autocomplete = opt.IsAutocomplete;
+
+      this.MinLength = opt.MinLength;
+      this.MaxLength = opt.MaxLength;
+
+      if (opt.Options != null && opt.Options?.Count > 0)
+      {
+        this.Options = opt.Options.Select(v => new DiscordCommandOption().Cast(v)).ToArray();
+      }
+
+      if (opt.Choices != null && opt.Choices?.Count > 0)
+      {
+        this.Choices = opt.Choices.Select(v => new DiscordCommandOptionChoice().Cast(v)).ToArray();
+      }
+
+      if (opt.ChannelTypes != null && opt.ChannelTypes.Count > 0)
+      {
+        this.ChannelTypes = opt.ChannelTypes.ToArray();
+      }
+      
+      return this;
+    }
+  }
+  public class DiscordCommandOptionChoice
+  {
+    [JsonPropertyName("name")]
+    public string Name { get; set; }
+
+    [JsonPropertyName("value")]
+    public object Value { get; set; }
+
+    public DiscordCommandOptionChoice Cast(RestApplicationCommandChoice choice)
+    {
+      this.Name = choice.Name;
+      this.Value = choice.Value;
+      return this;
+    }
+  }
--- XeniaBot.Shared/_Reflection/InteractionHandler.cs
+++ XeniaBot.Shared/_Reflection/InteractionHandler.cs
@@ -3,10 +3,15 @@ using Discord.WebSocket;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Reflection;
 using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using Discord.Rest;
 using XeniaBot.Shared;
 
 namespace XeniaBot.Data
@@ -16,11 +21,69 @@ namespace XeniaBot.Data
         private readonly InteractionService _interactionService;
         private readonly DiscordSocketClient _client;
         private readonly IServiceProvider _services;
+        private readonly ConfigData _configData;
         public InteractionHandler(IServiceProvider services)
         {
             _interactionService = services.GetRequiredService<InteractionService>();
             _client = services.GetRequiredService<DiscordSocketClient>();
+            _configData = services.GetRequiredService<ConfigData>();
             _services = services;
+        }
+
+        public async Task PostDBL(IReadOnlyCollection<RestGlobalCommand> data)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(_configData.DiscordBotList_Token))
+                {
+                    Log.WriteLine($"Ignoring since DiscordBotList_Token is empty");
+                    return;
+                }
+                var blacklist = new Dictionary<string, string[]>()
+                {
+                    {
+                        "auth", new string[]
+                        {
+                            "enableguild", "setguildstate"
+                        }
+                    },
+                    {
+                        "auth", Array.Empty<string>()
+                    },
+                    {
+                        "fetch_config", Array.Empty<string>()
+                    },
+                    {
+                        "metricreload", Array.Empty<string>()
+                    }
+                };
+                var casted = new List<DiscordApplicationCommand>();
+                foreach (var item in data)
+                    if (!blacklist.ContainsKey(item.Name))
+                        casted.Add(new DiscordApplicationCommand().Cast(item, blacklist));
+
+                var request = new HttpRequestMessage()
+                {
+                    RequestUri = new Uri($"https://discordbotlist.com/api/v1/bots/{_client.CurrentUser.Id}/commands"),
+                    Method = HttpMethod.Post,
+                };
+                request.Headers.Add("Authorization", $"Bot {_configData.DiscordBotList_Token}");
+                var ser = JsonSerializer.Serialize(
+                    casted, new JsonSerializerOptions()
+                    {
+                        IncludeFields = true,
+                        IgnoreReadOnlyFields = true,
+                        DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+                    });
+                request.Content = new StringContent(ser, null, "application/json");
+                var client = new HttpClient();
+                var res = await client.SendAsync(request);
+                Log.Debug($"DBL Response: {res.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failed to send commands to DiscordBotList.com\n{ex}");
+            }
         }
         public async Task InitializeAsync()
         {
@@ -29,8 +92,9 @@ namespace XeniaBot.Data
             {
                 mods = mods.Concat(_interactionService.AddModulesAsync(item, _services).Result).ToArray();
             }
-            await _interactionService.RegisterCommandsGloballyAsync();
-
+            var result = await _interactionService.RegisterCommandsGloballyAsync();
+            await PostDBL(result);
+            
             var lines = new List<string>();
             foreach (var item in mods)
             {
