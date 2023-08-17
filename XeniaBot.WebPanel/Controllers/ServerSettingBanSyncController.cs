aaba65fbc7a36eb08d284735521a20ec1462dfea
kate <kate@dariox.club>
2023-08-17 20:09:18 +0800

    Add confession config

--- XeniaBot.WebPanel/Controllers/ServerSettingBanSyncController.cs
+++ XeniaBot.WebPanel/Controllers/ServerSettingBanSyncController.cs
@@ -129,10 +129,6 @@ public partial class ServerController
         if (!CanAccess(id))
             return View("NotAuthorized");
         
-        var userId = AspHelper.GetUserId(HttpContext);
-        if (userId == null)
-            return View("NotFound", "User not found");
-        var user = _discord.GetUser((ulong)userId);
         var guild = _discord.GetGuild(id);
         if (guild == null)
             return View("NotFound", "Guild not found");
@@ -156,5 +152,98 @@ public partial class ServerController
             MessageType = "success",
             Message = "Successfully set Ban Sync log channel"
         });
+    }
+    [HttpPost("~/Server/{id}/Settings/Confession")]
+    public async Task<IActionResult> SaveSettings_Confession(ulong id, string? modalChannelId, string? messageChannelId)
+    {
+        if (!CanAccess(id))
+            return View("NotAuthorized");
+        
+        var guild = _discord.GetGuild(id);
+        if (guild == null)
+            return View("NotFound", "Guild not found");
+        
+        var channelIdRes_modal = ParseChannelId(id, modalChannelId, out var modalId);
+        if (channelIdRes_modal != null)
+            return channelIdRes_modal;
+        
+        var channelIdRes_msg = ParseChannelId(id, modalChannelId, out var msgId);
+        if (channelIdRes_msg != null)
+            return channelIdRes_msg;
+
+        try
+        {
+            var controller = Program.Services.GetRequiredService<ConfessionConfigController>();
+            var data = await controller.GetGuild(id) ??
+                       new ConfessionGuildModel()
+                       {
+                           GuildId = id
+                       };
+            if (data.ModalChannelId != modalId)
+            {
+                await controller.InitializeModal(id, msgId, modalId);
+            }
+
+            data = await controller.GetGuild(id) ??
+                   new ConfessionGuildModel()
+                   {
+                       GuildId = id
+                   };
+            data.ModalChannelId = modalId;
+            data.ChannelId = msgId;
+            await controller.Set(data);
+        }
+        catch (Exception ex)
+        {
+            return RedirectToAction("Index", new
+            {
+                Id = id,
+                MessageType = "danger",
+                Message = $"Failed to save confession settings: {ex.Message}"
+            });
+        }
+        return RedirectToAction("Index", new
+        {
+            Id = id,
+            MessageType = "success",
+            Message = "Successfully set Ban Sync log channel"
+        });
+    }
+
+    [HttpGet("~/Server/{id}/Settings/Confession/Purge")]
+    public async Task<IActionResult> SaveSettings_Confession_Purge(ulong id)
+    {
+        if (!CanAccess(id))
+            return View("NotAuthorized");
+        
+        var guild = _discord.GetGuild(id);
+        if (guild == null)
+            return View("NotFound", "Guild not found");
+
+        try
+        {
+            var controller = Program.Services.GetRequiredService<ConfessionConfigController>();
+            var data = await controller.GetGuild(id)
+                ?? new ConfessionGuildModel()
+                {
+                   GuildId = id
+                };
+            await controller.Delete(data);
+            return RedirectToAction("Index", new
+            {
+                Id = id,
+                MessageType = "success",
+                Message = "Successfully purged confession messages"
+            });
+        }
+        catch (Exception ex)
+        {
+            return RedirectToAction("Index", new
+            {
+                Id = id,
+                MessageType = "danger",
+                Message = $"Failed to purge confession messages: {ex.Message}"
+            });
+        }
     }
 }
