1e70f8da0ec8071a821d5b6318894a90f7fb21a2
kate <kate@dariox.club>
2023-08-21 19:09:17 +0800

    Create GuildPrefix Config

---
+++ XeniaBot.Shared/Controllers/GuildPrefixConfigController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+using XeniaBot.Shared.Models;
+
+namespace XeniaBot.Shared.Controllers;
+
+[BotController]
+public class GuildPrefixConfigController : BaseConfigController<GuildPrefixConfigModel>
+{
+    private readonly ConfigData _config;
+    public GuildPrefixConfigController(IServiceProvider services)
+        : base("guildPrefix", services)
+    {
+        _config = services.GetRequiredService<ConfigData>();
+    }
+
+    public async Task<string> GetPrefix(ulong? guildId)
+    {
+        if (guildId == null)
+            return _config.Prefix;
+        var data = await Get((ulong)guildId);
+        return data?.Prefix ?? _config.Prefix;
+    }
+
+    public async Task<GuildPrefixConfigModel> Get(ulong guildId)
+    {
+        var collection = GetCollection();
+        var defaultData = new GuildPrefixConfigModel(guildId);
+        var result = await collection.FindAsync(defaultData.Filter);
+        return result.FirstOrDefault() ?? defaultData;
+    }
+
+    public async Task<bool> Exists(ulong guildId)
+    {
+        var collection = GetCollection();
+        var result = await collection.FindAsync(new GuildPrefixConfigModel(guildId).Filter);
+        return result?.Any() ?? false;
+    }
+    public async Task Set(GuildPrefixConfigModel data)
+    {
+        var collection = GetCollection();
+        if (await Exists(data.GuildId))
+        {
+            await collection.ReplaceOneAsync(data.Filter, data);
+        }
+        else
+        {
+            await collection.InsertOneAsync(data);
+        }
+    }
+}
---
+++ XeniaBot.Shared/Models/GuildPrefixConfigModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Xml.Serialization;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Newtonsoft.Json;
+
+namespace XeniaBot.Shared.Models;
+
+public class GuildPrefixConfigModel : BaseModel
+{
+    public ulong GuildId { get; set; }
+    public string? Prefix { get; set; }
+
+    public GuildPrefixConfigModel()
+    {
+        GuildId = 0;
+        Prefix = null;
+    }
+
+    [BsonIgnore]
+    [JsonIgnore]
+    [XmlIgnore]
+    public FilterDefinition<GuildPrefixConfigModel> Filter =>
+        Builders<GuildPrefixConfigModel>.Filter.Eq("GuildId", GuildId);
+    public GuildPrefixConfigModel(ulong guildId)
+    {
+        GuildId = guildId;
+        Prefix = null;
+    }
+}
