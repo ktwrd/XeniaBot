e4f11dc88cde2f0d90d7d510a0f80b21b672a7dd
kate <kate@dariox.club>
2023-08-24 20:52:20 +0800

    Create BaseView Model

---
+++ XeniaBot.WebPanel/Models/BaseViewModel.cs
@@ -0,0 +1,13 @@
+﻿using Discord.WebSocket;
+using XeniaBot.Data.Models;
+
+namespace XeniaBot.WebPanel.Models;
+
+public class BaseViewModel
+{
+    public DiscordSocketClient Client { get; set; }
+    public UserConfigModel UserConfig { get; set; }
+    
+    public string? MessageType { get; set; }
+    public string? Message { get; set; }
+}
