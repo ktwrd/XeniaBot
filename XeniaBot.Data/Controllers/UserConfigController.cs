da3a43db65b917e8dded50a6935d5384fb72e980
kate <kate@dariox.club>
2023-08-24 20:38:00 +0800

    Add UserConfig Model/Controller

---
+++ XeniaBot.Data/Controllers/UserConfigController.cs
@@ -0,0 +1,45 @@
+﻿using MongoDB.Driver;
+using XeniaBot.Data.Models;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Data.Controllers;
+
+[BotController]
+public class UserConfigController : BaseConfigController<UserConfigModel>
+{
+    public UserConfigController(IServiceProvider services)
+        : base("userConfig", services)
+    {
+    }
+
+    public async Task<UserConfigModel?> Get(ulong? id)
+    {
+        if (id == null)
+        {
+            return new UserConfigModel();
+        }
+        var filter = Builders<UserConfigModel>
+            .Filter
+            .Eq("UserId", id);
+        var collection = GetCollection();
+        var result = await collection.FindAsync(filter);
+        var sorted = result.ToList().OrderByDescending(v => v.ModifiedAtTimestamp);
+        return sorted.FirstOrDefault();
+    }
+
+    public async Task<UserConfigModel> GetOrDefault(ulong id)
+    {
+        var res = await Get(id);
+        return res
+            ?? new UserConfigModel()
+            {
+                UserId = id
+            };
+    }
+
+    public async Task Add(UserConfigModel model)
+    {
+        var collection = GetCollection();
+        await collection.InsertOneAsync(model);
+    }
+}
---
+++ XeniaBot.Data/Models/UserConfigModel.cs
@@ -0,0 +1,26 @@
+﻿using XeniaBot.Shared.Models;
+
+namespace XeniaBot.Data.Models;
+
+public class UserConfigModel : BaseModel
+{
+    public ulong UserId { get; set; }
+    public long ModifiedAtTimestamp { get; set; }
+    
+    public bool EnableProfileTracking { get; set; }
+    public ListViewStyle ListViewStyle { get; set; }
+
+    public void Defaults()
+    {
+        UserConfigModel.Defaults(this);
+    }
+
+    public static void Defaults(UserConfigModel model)
+    {
+        model.EnableProfileTracking = true;
+        model.ListViewStyle = ListViewStyle.List;
+    }
+    
+    public UserConfigModel()
+    { Defaults(); }
+}
