4f29604737694612fb4edf164f04c88d32552c51
kate <kate@dariox.club>
2023-08-24 20:52:31 +0800

    Create FormCheckboxView Model

---
+++ XeniaBot.WebPanel/Models/FormCheckboxViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace XeniaBot.WebPanel.Models;
+
+public class FormCheckboxViewModel
+{
+    public string ParentFormId { get; set; }
+    public string Name { get; set; }
+    public string Id { get; set; }
+    public string Label { get; set; }
+    public bool State { get; set; }
+    public bool Margin { get; set; } = false;
+}
