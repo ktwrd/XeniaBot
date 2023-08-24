cfb131ecf4e45583dd1e28566e89f32615c2a4bf
kate <kate@dariox.club>
2023-08-24 20:52:46 +0800

    Add 403 route

---
+++ XeniaBot.WebPanel/Controllers/RootController.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using XeniaBot.WebPanel.Models;
+
+namespace XeniaBot.WebPanel.Controllers;
+
+[Route("/")]
+[Controller]
+public class RootController : Controller
+{
+    [HttpGet("/403")]
+    public IActionResult NotAuth(NotAuthorizedViewModel model)
+    {
+        return View("NotAuthorized", model);
+    }
+}
---
+++ XeniaBot.WebPanel/Models/NotAuthorizedViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace XeniaBot.WebPanel.Models;
+
+public class NotAuthorizedViewModel
+{
+    public string? Message { get; set; }
+    public bool ShowLoginButton { get; set; }
+
+    public NotAuthorizedViewModel()
+    {
+        ShowLoginButton = false;
+    }
+}
