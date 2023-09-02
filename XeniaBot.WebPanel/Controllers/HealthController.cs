a59b469fd57242525028bf3fb2690727f0de4d20
kate <kate@dariox.club>
2023-09-02 19:39:11 +0800

    Add health endpoint

---
+++ XeniaBot.WebPanel/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XeniaBot.WebPanel.Controllers;
+
+public class HealthController : BaseXeniaController
+{
+    public HealthController()
+        : base()
+    {
+    }
+
+    [HttpGet("/Health")]
+    public IActionResult Health()
+    {
+        var data = new Dictionary<string, object>()
+        {
+            {"StartTimestamp", Program.StartTimestamp},
+            {"Version", Program.Version},
+            {"ServiceName", "XeniaDiscordBot"}
+        };
+        return Json(data, Program.SerializerOptions);
+    }
+}
