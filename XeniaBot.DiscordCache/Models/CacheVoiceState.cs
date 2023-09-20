02bf2bec0c1d67d03d29a96ab49905068d202213
kate <kate@dariox.club>
2023-09-20 13:03:54 +0800

    Create CacheVoiceState

---
+++ XeniaBot.DiscordCache/Models/CacheVoiceState.cs
@@ -0,0 +1,95 @@
+﻿using Discord;
+
+namespace XeniaBot.DiscordCache.Models;
+
+/// <summary>
+///     Represents a WebSocket user's voice connection status. Mutable version of <see cref="IVoiceState"/>
+/// </summary>
+public class CacheVoiceState
+{
+    /// <summary>Gets the voice channel this user is currently in.</summary>
+    /// <returns>
+    ///     A generic voice channel object representing the voice channel that the user is currently in; <see langword="null" />
+    ///     if none.
+    /// </returns>
+    public ulong VoiceChannelId { get; set; }
+    /// <summary>
+    ///     Gets the unique identifier for this user's voice session.
+    /// </summary>
+    public string VoiceSessionId { get; set; }
+    /// <summary>
+    ///     Gets the time on which the user requested to speak.
+    /// </summary>
+    public DateTimeOffset? RequestToSpeakTimestamp { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates whether this user is muted (i.e. not permitted to speak via voice) by the
+    ///     guild.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if this user is muted by the guild; otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsMuted { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates whether this user is deafened by the guild.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if the user is deafened (i.e. not permitted to listen to or speak to others) by the guild;
+    ///     otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsDeafened { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates whether the user is muted by the current user.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if the guild is temporarily blocking audio to/from this user; otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsSuppressed { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates whether this user has marked themselves as muted (i.e. not permitted to
+    ///     speak via voice).
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if this user has muted themselves; otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsSelfMuted { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates whether this user has marked themselves as deafened.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if this user has deafened themselves (i.e. not permitted to listen to or speak to others); otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsSelfDeafened { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates if this user is streaming in a voice channel.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if the user is streaming; otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsStreaming { get; set; }
+    /// <summary>
+    ///     Gets a value that indicates if the user is videoing in a voice channel.
+    /// </summary>
+    /// <returns>
+    ///     <see langword="true" /> if the user has their camera turned on; otherwise <see langword="false" />.
+    /// </returns>
+    public bool IsVideoing { get; set; }
+
+    public static CacheVoiceState? FromVoiceState(IVoiceState? state)
+    {
+        if (state == null)
+            return null;
+
+        var instance = new CacheVoiceState();
+        instance.VoiceChannelId = state.VoiceChannel.Id;
+        instance.VoiceSessionId = state.VoiceSessionId;
+        instance.RequestToSpeakTimestamp = state.RequestToSpeakTimestamp;
+        instance.IsMuted = state.IsMuted;
+        instance.IsDeafened = state.IsDeafened;
+        instance.IsSuppressed = state.IsSuppressed;
+        instance.IsSelfMuted = state.IsSelfMuted;
+        instance.IsSelfDeafened = state.IsSelfDeafened;
+        instance.IsStreaming = state.IsStreaming;
+        instance.IsVideoing = state.IsVideoing;
+        return instance;
+    }
+}
