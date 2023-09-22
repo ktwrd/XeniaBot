31aa6deabe2f9f3578ac1d0634a2b61671b2772c
kate <kate@dariox.club>
2023-09-22 10:51:35 +0800

    Update CacheMessageReference.cs

--- XeniaBot.DiscordCache/Models/Message/CacheMessageReference.cs
+++ XeniaBot.DiscordCache/Models/Message/CacheMessageReference.cs
@@ -5,7 +5,7 @@ namespace XeniaBot.DiscordCache.Models;
 public class CacheMessageReference
 {
     public ulong MessageId { get; set; }
-    public ulong ChannelID { get; set; }
+    public ulong ChannelId { get; set; }
     public ulong GuildId { get; set; }
     public bool FailIfNotExists { get; set; }
 
@@ -14,7 +14,7 @@ public class CacheMessageReference
         return new CacheMessageReference()
         {
             MessageId = (r?.MessageId.IsSpecified ?? false) ? r?.MessageId.Value ?? 0 : 0,
-            ChannelID = r != null && r?.ChannelId != null ? r?.ChannelId ?? 0 : 0,
+            ChannelId = r?.ChannelId ?? 0,
             GuildId = (r?.GuildId.IsSpecified ?? false) ? r?.GuildId.Value ?? 0 : 0,
             FailIfNotExists = (r?.FailIfNotExists.IsSpecified ?? false) || (r?.FailIfNotExists.Value ?? true)
         };
