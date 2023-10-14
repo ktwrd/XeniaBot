a0411ce70636e414a8e7fd51bc93b506e9daccab
kate <kate@dariox.club>
2023-10-14 15:34:21 +0800

    Add view model for WarnGuildDetails

---
+++ XeniaBot.WebPanel/Models/Pages/WarnGuildDetailsViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Discord.WebSocket;
+using XeniaBot.Data.Models;
+
+namespace XeniaBot.WebPanel.Models;
+
+public class WarnGuildDetailsViewModel : BaseViewModel, IBaseServerModel
+{
+    public SocketGuildUser User { get; set; }
+    public SocketGuild Guild { get; set; }
+    public ICollection<SocketGuildUser> UsersWhoCanAccess { get; set; }
+    
+    public CounterGuildModel CounterConfig { get; set; }
+    public ConfigBanSyncModel BanSyncConfig { get; set; }
+    public LevelSystemGuildConfigModel XpConfig { get; set; }
+    public ServerLogModel LogConfig { get; set; }
+    public ICollection<BanSyncStateHistoryItemModel> BanSyncStateHistory { get; set; }
+    public GuildGreeterConfigModel GreeterConfig { get; set; }
+    public GuildByeGreeterConfigModel GreeterGoodbyeConfig { get; set; }
+    public ICollection<GuildWarnItemModel> WarnItems { get; set; }
+}
