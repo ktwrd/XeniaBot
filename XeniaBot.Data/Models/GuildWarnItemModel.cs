245e0368f3b2cd56216066ed73b05714654147e6
kate <kate@dariox.club>
2023-10-14 15:32:36 +0800

    Add GuildWarnItemModel

---
+++ XeniaBot.Data/Models/GuildWarnItemModel.cs
@@ -0,0 +1,29 @@
+﻿using kate.shared.Helpers;
+using XeniaBot.Shared.Models;
+
+namespace XeniaBot.Data.Models;
+
+public class GuildWarnItemModel : BaseModel
+{
+    public static string CollectionName => "guildWarnItems";
+    
+    public string WarnId { get; set; }
+    public ulong GuildId { get; set; }
+    /// <summary>
+    /// User Id for who this warn is for
+    /// </summary>
+    public ulong TargetUserId { get; set; }
+    /// <summary>
+    /// User Id for who warned <see cref="TargetUserId"/>
+    /// </summary>
+    public ulong ActionedUserId { get; set; }
+    public long ModifiedAtTimestamp { get; set; }
+    public long CreatedAtTimestamp { get; set; }
+    public string Description { get; set; }
+
+    public GuildWarnItemModel()
+        : base()
+    {
+        WarnId = Guid.NewGuid().ToString();
+    }
+}
