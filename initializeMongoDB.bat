26c15b2b040e17472da6cd3efb0dbecd9c69c191
kate <kate@dariox.club>
2023-10-31 14:30:11 +0800

    Update initialize scripts

--- initializeMongoDB.bat
+++ initializeMongoDB.bat
@@ -1,5 +1,5 @@
 @echo off
-echo Creating database with name of "mongodb-shortcake"
+echo Creating database with name of "mongodb-xenia"
 echo Username: user
 echo Password: password
-docker run -v mongoData:/data/db -p 27020:27017 --name mongodb-shortcake -d -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=password mongo
+docker run -v mongoData:/data/db -p 27020:27017 --name mongodb-xenia -d -e MONGO_INITDB_ROOT_USERNAME=user -e MONGO_INITDB_ROOT_PASSWORD=password mongo
--- initializeMongoDB.sh
+++ initializeMongoDB.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-echo "Creating database with name of mongodb-shortcake"
+echo "Creating database with name of mongodb-xenia"
 echo "Username: user"
 echo "Password: password"
 echo "Port: 27020"
 docker run \
 	-v mongoData:/data/db \
 	-p 27020:27017 \
-	--name mongodb-shortcake \
+	--name mongodb-xenia \
 	-d \
 	-e MONGO_INITDB_ROOT_USERNAME=user \
 	-e MONGO_INITDB_ROOT_PASSWORD=password mongo
