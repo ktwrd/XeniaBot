fea47626f60dffbd3ee6c1513b5569bfa9623e51
kate <kate@dariox.club>
2023-11-04 21:53:16 +0800

    Add component to render markdown

---
+++ XeniaBot.WebPanel/Models/Component/RenderMarkdownViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace XeniaBot.WebPanel.Models;
+
+public class RenderMarkdownViewModel
+{
+    /// <summary>
+    /// Markdown Content to render as HTML
+    /// </summary>
+    public string Content { get; set; }
+}
---
+++ XeniaBot.WebPanel/ViewComponents/RenderMarkdownViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XeniaBot.WebPanel.Models;
+
+namespace XeniaBot.WebPanel.ViewComponents;
+
+public class RenderMarkdownViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(string data)
+    {
+        return View("Default", new RenderMarkdownViewModel()
+        {
+            Content = data
+        });
+    }
+}
