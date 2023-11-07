b76d81b334d1e3f755b16c758fb3e2b58c9fcaa0
kate <kate@dariox.club>
2023-11-07 15:53:12 +0800

    Add fields to show import/export button

--- XeniaBot.WebPanel/Models/Component/EmbedViewerModel.cs
+++ XeniaBot.WebPanel/Models/Component/EmbedViewerModel.cs
@@ -3,4 +3,13 @@
 public class EmbedViewerModel
 {
     public string Id { get; set; }
+    public bool ShowImportButton { get; set; }
+    public bool ShowExportButton { get; set; }
+
+    public EmbedViewerModel(string id)
+    {
+        Id = id;
+        ShowImportButton = false;
+        ShowExportButton = false;
+    }
 }
