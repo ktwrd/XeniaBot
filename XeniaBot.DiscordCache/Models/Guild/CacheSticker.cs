a84e78501b4048b2004eb0ca92275842a70c7d7a
kate <kate@dariox.club>
2023-11-22 15:46:58 +0800

    Update CacheSticker.cs

--- XeniaBot.DiscordCache/Models/Guild/CacheSticker.cs
+++ XeniaBot.DiscordCache/Models/Guild/CacheSticker.cs
@@ -4,25 +4,19 @@ namespace XeniaBot.DiscordCache.Models;
 
 public class CacheSticker : CacheStickerItem
 {
-    public new ulong StickerId { get; set; }
     public ulong PackId { get; set; }
-    public new string Name { get; set; }
     public string Description { get; set; }
     public string[] Tags { get; set; }
     public StickerType Type { get; set; }
-    public new StickerFormatType Format { get; set; }
     public bool? IsAvailable { get; set; }
     public int? SortOrder { get; set; }
     public CacheSticker Update(ISticker sticker)
     {
         base.Update(sticker);
-        StickerId = sticker.Id;
         PackId = sticker.PackId;
-        Name = sticker.Name;
         Description = sticker.Description;
         Tags = sticker.Tags.ToArray();
         Type = sticker.Type;
-        Format = sticker.Format;
         IsAvailable = sticker.IsAvailable;
         SortOrder = sticker.SortOrder;
         return this;
