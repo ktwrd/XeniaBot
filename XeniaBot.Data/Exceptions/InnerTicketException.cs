85b16c7d1b07919c468e4b077a6be75a6a623e24
kate <kate@dariox.club>
2023-12-09 13:10:41 +0800

    Create InnerTicketException

---
+++ XeniaBot.Data/Exceptions/InnerTicketException.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using XeniaBot.Data.Models;
+
+namespace XeniaBot.Data;
+
+public class InnerTicketException : Exception
+{
+    #region Constructors
+    public InnerTicketException(TicketModel ticketModel)
+        : base()
+    {
+        Ticket = ticketModel;
+    }
+
+    public InnerTicketException(string? message, TicketModel ticketModel)
+        : base(message)
+    {
+        Ticket = ticketModel;
+    }
+
+    public InnerTicketException(string? message, TicketModel ticketModel, Exception? innerException)
+        : base(message, innerException)
+    {
+        Ticket = ticketModel;
+    }
+    #endregion
+    
+    public TicketModel Ticket { get; private set; }
+
+    public override string ToString()
+    {
+        return base.ToString();
+    }
+}
