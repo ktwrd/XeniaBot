e4572196200ad30e893ba84b96471859426f1942
kate <kate@dariox.club>
2023-12-12 13:20:30 +0800

    Add method CanAccessChannel
    Checks if CurrentUser can actually view/send messages in the given channel.

--- XeniaBot.Core/Helpers/DiscordHelper.cs
+++ XeniaBot.Core/Helpers/DiscordHelper.cs
@@ -16,6 +16,19 @@ namespace XeniaBot.Core.Helpers
 {
     public static class DiscordHelper
     {
+        /// <summary>
+        /// Check if <paramref name="channel"/> has the ViewChannel, SendMessages, and EmbedLinks permissions.
+        /// </summary>
+        /// <returns>If <paramref name="client"/> has access to <paramref name="channel"/></returns>
+        public static bool CanAccessChannel(DiscordSocketClient client, SocketGuildChannel channel)
+        {
+            var currentGuildUser = channel.Guild.GetUser(client.CurrentUser.Id);
+            var channelPermissions = currentGuildUser.GetPermissions(channel);
+            return channelPermissions.Has(ChannelPermission.ViewChannel) &&
+                   channelPermissions.Has(ChannelPermission.SendMessages) &&
+                   channelPermissions.Has(ChannelPermission.EmbedLinks);
+        }
+        
         public static EmbedBuilder BaseEmbed(EmbedBuilder? embed=null)
         {
             if (embed == null)
