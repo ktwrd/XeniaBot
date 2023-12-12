1296f8587ec21a29a72a60bc1cf82e649f5a1b58
kate <kate@dariox.club>
2023-12-12 13:21:46 +0800

    Create DiscordURLHelper

--- XeniaBot.Shared/Helpers/DiscordURLHelper.cs
+++ XeniaBot.Shared/Helpers/DiscordURLHelper.cs
@@ -5,5 +5,10 @@ public static class DiscordURLHelper
     public const string UrlPrefix = "https://discord.com";
 
     public static string GuildChannelMessage(ulong guildId, ulong channelId, ulong messageId) =>
-        $"{UrlPrefix}/channels/{guildId}/{channelId}/{messageId}";
+        $"{GuildChannel(guildId, channelId)}/{messageId}";
+
+    public static string GuildChannel(ulong guildId, ulong channelId) =>
+        $"{Guild(guildId)}/{channelId}";
+
+    public static string Guild(ulong guildId) => $"{UrlPrefix}/channels/{guildId}";
 }
