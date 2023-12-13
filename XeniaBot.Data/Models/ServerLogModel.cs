b470a9218e58833292d212e7d1821eca3023faea
kate <kate@dariox.club>
2023-12-13 17:55:00 +0800

    Add function comments

--- XeniaBot.Data/Models/ServerLogModel.cs
+++ XeniaBot.Data/Models/ServerLogModel.cs
@@ -34,6 +34,11 @@ public class ServerLogModel : BaseModel
     public ulong? ChannelDeleteChannel = null;
     public ulong? MemberVoiceChangeChannel = null;
 
+    /// <summary>
+    /// Get a specific channel. When `null`, it will default to <see cref="DefaultLogChannel"/>.
+    /// </summary>
+    /// <param name="logEvent"></param>
+    /// <returns>When `null` or `0` then do not send messages for this event.</returns>
     public ulong GetChannel(ServerLogEvent logEvent)
     {
         var dict = GetAsDictionary();
@@ -61,9 +66,14 @@ public class ServerLogModel : BaseModel
         };
     }
 
+    /// <summary>
+    /// Set the Log Channel for a specific event.
+    /// </summary>
+    /// <param name="logEvent">Log Event to set <paramref name="channelId"/> to.</param>
+    /// <param name="channelId">When `null`, then <see cref="DefaultLogChannel"/> will be used when fetching. If you want this event to not be logged, then use `0` as this parameter.</param>
+    /// <exception cref="Exception">Thrown when <paramref name="logEvent"/> isn't implemented in this function.</exception>
     public void SetChannel(ServerLogEvent logEvent, ulong? channelId)
     {
-        
         switch (logEvent)
         {
             case ServerLogEvent.Fallback:
