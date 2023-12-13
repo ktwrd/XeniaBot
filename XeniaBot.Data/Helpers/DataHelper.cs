143eacea7853f30365b47254f73478eab282e47c
kate <kate@dariox.club>
2023-12-13 17:53:24 +0800

    Port DiscordHelper.CanAccessChannel to DataHelper

---
+++ XeniaBot.Data/Helpers/DataHelper.cs
@@ -0,0 +1,22 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace XeniaBot.Data.Helpers;
+
+public static class DataHelper
+{
+    
+    /// <summary>
+    /// Check if <paramref name="channel"/> has the ViewChannel, SendMessages, and EmbedLinks permissions.
+    /// </summary>
+    /// <returns>If <paramref name="client"/> has access to <paramref name="channel"/></returns>
+    public static bool CanAccessChannel(DiscordSocketClient client, SocketGuildChannel channel)
+    {
+        var currentGuildUser = channel.Guild.GetUser(client.CurrentUser.Id);
+        var channelPermissions = currentGuildUser.GetPermissions(channel);
+        return channelPermissions.Has(ChannelPermission.ViewChannel) &&
+               channelPermissions.Has(ChannelPermission.SendMessages) &&
+               channelPermissions.Has(ChannelPermission.EmbedLinks) &&
+               channelPermissions.Has(ChannelPermission.ReadMessageHistory);
+    }
+}
