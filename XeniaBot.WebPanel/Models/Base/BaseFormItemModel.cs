050fdc9ad4e0b4dd5cdf040b2af25218c01e64a0
kate <kate@dariox.club>
2023-12-27 15:11:08 +0800

    Create BaseFormItemModel.cs

---
+++ XeniaBot.WebPanel/Models/Base/BaseFormItemModel.cs
@@ -0,0 +1,17 @@
+﻿namespace XeniaBot.WebPanel.Models;
+
+public class BaseFormItemModel
+{
+    /// <summary>
+    /// `form` attribute value on `select` element.
+    /// </summary>
+    public string ParentFormId { get; set; }
+    /// <summary>
+    /// `name` attribute value on `select` element.
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// `id` attribute value on `select` element.
+    /// </summary>
+    public string Id { get; set; }
+}
