f7e4b9273bfe8585b3c8b90fbc3cd2677bf54a12
kate <kate@dariox.club>
2023-12-27 15:11:00 +0800

    Move LevelSystem routes to own file

--- XeniaBot.WebPanel/Controllers/ServerSettingsController.cs
+++ XeniaBot.WebPanel/Controllers/ServerSettingsController.cs
@@ -110,58 +110,6 @@ public partial class ServerController
             message: $"Logging settings saved");
     }
 
-    [HttpPost("~/Server/{id}/Settings/Xp")]
-    public async Task<IActionResult> SaveSettings_Xp(ulong id, string? channelId, bool show, bool enable)
-    {
-        if (!CanAccess(id))
-            return View("NotAuthorized");
-        
-        var guild = _discord.GetGuild(id);
-        if (guild == null)
-            return View("NotFound", "Guild not found");
-
-        ulong? targetChannelId = null;
-        try
-        {
-            if (channelId == null)
-                targetChannelId = null;
-            else
-                targetChannelId = ulong.Parse(channelId);
-        }
-        catch (Exception e)
-        {
-            return await Index(id,
-                messageType: "danger",
-                message: $"Failed to save Level System settings. {e.Message}");
-        }
-
-        try
-        {
-            var controller = Program.Services.GetRequiredService<LevelSystemGuildConfigController>();
-            var data = await controller.Get(guild.Id) ?? new LevelSystemGuildConfigModel()
-            {
-                GuildId = guild.Id,
-                LevelUpChannel = targetChannelId,
-                ShowLeveUpMessage = show,
-                Enable = enable
-            };
-            data.LevelUpChannel = targetChannelId;
-            data.ShowLeveUpMessage = show;
-            data.Enable = enable;
-            await controller.Set(data);
-            return await Index(id,
-                messageType: "success",
-                message: $"Level System Settings Saved");
-        }
-        catch (Exception e)
-        {
-            Log.Error($"Failed to save level system config\n{e}");
-            return await Index(id,
-                messageType: "danger",
-                message: $"Failed to save Level System settings. {e.Message}");
-        }
-    }
-
     [HttpPost("~/Server/{id}/Settings/Greeter")]
     public async Task<IActionResult> SaveSettings_Greeter(
         ulong id,
---
+++ XeniaBot.WebPanel/Controllers/ServerSettingsXpController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XeniaBot.Data.Controllers.BotAdditions;
+using XeniaBot.Data.Models;
+using XeniaBot.Shared;
+
+namespace XeniaBot.WebPanel.Controllers;
+
+public partial class ServerController
+{
+
+    [HttpPost("~/Server/{id}/Settings/Xp/Save")]
+    public async Task<IActionResult> SaveSettings_Xp(ulong id, string? channelId, bool show, bool enable)
+    {
+        if (!CanAccess(id))
+            return View("NotAuthorized");
+        
+        var guild = _discord.GetGuild(id);
+        if (guild == null)
+            return View("NotFound", "Guild not found");
+
+        ulong? targetChannelId;
+        try
+        {
+            if (channelId == null)
+                targetChannelId = null;
+            else
+                targetChannelId = ulong.Parse(channelId);
+        }
+        catch (Exception e)
+        {
+            return await Index(id,
+                messageType: "danger",
+                message: $"Failed to save Level System settings. {e.Message}");
+        }
+
+        try
+        {
+            var controller = Program.Services.GetRequiredService<LevelSystemGuildConfigController>();
+            var data = await controller.Get(guild.Id) ?? new LevelSystemGuildConfigModel()
+            {
+                GuildId = guild.Id,
+                LevelUpChannel = targetChannelId,
+                ShowLeveUpMessage = show,
+                Enable = enable
+            };
+            data.LevelUpChannel = targetChannelId;
+            data.ShowLeveUpMessage = show;
+            data.Enable = enable;
+            await controller.Set(data);
+            return await Index(id,
+                messageType: "success",
+                message: $"Level System Settings Saved");
+        }
+        catch (Exception e)
+        {
+            Log.Error($"Failed to save level system config\n{e}");
+            return await Index(id,
+                messageType: "danger",
+                message: $"Failed to save Level System settings. {e.Message}");
+        }
+    }
+
+}
