0987b2ef32d057c8978fc20c773cbc35230b1070
kate <kate@dariox.club>
2023-12-27 15:12:37 +0800

    Add ViewComponent for only the option part of ChannelSelect

---
+++ XeniaBot.WebPanel/ViewComponents/ChannelSelectRawViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using XeniaBot.WebPanel.Models;
+
+namespace XeniaBot.WebPanel.ViewComponents;
+
+public class ChannelSelectRawViewComponent : ViewComponent
+{
+    /// <summary>
+    /// Only generate the option elements for a select.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync(ChannelSelectModel data)
+    {
+        return View("Default", data);
+    }
+}
