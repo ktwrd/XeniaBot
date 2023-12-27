9800a954fc4448e5c67147a17a903a7a09f99065
kate <kate@dariox.club>
2023-12-27 15:11:31 +0800

    Add comments

--- XeniaBot.WebPanel/ViewComponents/ChannelSelectViewComponent.cs
+++ XeniaBot.WebPanel/ViewComponents/ChannelSelectViewComponent.cs
@@ -5,6 +5,11 @@ namespace XeniaBot.WebPanel.ViewComponents;
 
 public class ChannelSelectViewComponent : ViewComponent
 {
+    /// <summary>
+    /// Generate whole ChannelSelect dropdown with form tags (like `name`, `id`, and `form` on the `select` element).
+    ///
+    /// Also contains everything in `input-group` to make it look lovely with bootstrap.
+    /// </summary>
     public async Task<IViewComponentResult> InvokeAsync(ChannelSelectModel data)
     {
         return View("Default", data);
