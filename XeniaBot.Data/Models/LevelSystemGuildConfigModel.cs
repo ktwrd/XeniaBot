58918d985146805bd1472da1cd150a14f7fc29f0
kate <kate@dariox.club>
2023-12-27 15:14:04 +0800

    Add field RoleGrant

--- XeniaBot.Data/Models/LevelSystemGuildConfigModel.cs
+++ XeniaBot.Data/Models/LevelSystemGuildConfigModel.cs
@@ -8,11 +8,29 @@ public class LevelSystemGuildConfigModel : BaseModel
     public ulong? LevelUpChannel { get; set; }
     public bool ShowLeveUpMessage { get; set; }
     public bool Enable { get; set; }
+    
+    /// <summary>
+    /// Role rewards to grant user when they reach a milestone.
+    /// </summary>
+    public List<LevelSystemRoleGrantItem> RoleGrant { get; set; }
 
     public LevelSystemGuildConfigModel()
     {
         LevelUpChannel = null;
         Enable = true;
         ShowLeveUpMessage = true;
+        RoleGrant = new List<LevelSystemRoleGrantItem>();
     }
+}
+
+public class LevelSystemRoleGrantItem
+{
+    /// <summary>
+    /// RoleId to grant to member when they reach the calculated level of <see cref="RequiredLevel"/>.
+    /// </summary>
+    public ulong RoleId { get; set; }
+    /// <summary>
+    /// Minimum required level to have <see cref="RoleId"/> granted to them.
+    /// </summary>
+    public ulong RequiredLevel { get; set; }
 }
