13008de952bc00c9a2bc7282f13aba63e399a6d4
kate <kate@dariox.club>
2023-12-27 15:12:46 +0800

    Add StrippedRole

---
+++ XeniaBot.WebPanel/Models/Wrappers/StrippedRole.cs
@@ -0,0 +1,67 @@
+﻿using Discord.WebSocket;
+
+namespace XeniaBot.WebPanel.Models;
+
+public class StrippedRole
+{
+    /// <summary>
+    /// Hex Color of <see cref="SocketRole.Color"/>. Default is `#00000000`
+    /// </summary>
+    public string HexColor { get; set; }
+    /// <summary>
+    /// Id of role. Cloned from <see cref="SocketRole.Id"/>
+    /// </summary>
+    public ulong RoleId { get; set; }
+    /// <summary>
+    /// When the role was created. <see cref="SocketRole.CreatedAt"/>
+    /// </summary>
+    public DateTimeOffset CreatedAt { get; set; }
+    /// <summary>
+    /// Name of the role. <see cref="SocketRole.Name"/>
+    /// </summary>
+    public string Name { get; set; }
+    /// <summary>
+    /// Position of the role. <see cref="SocketRole.Position"/>
+    /// </summary>
+    public int Position { get; set; }
+    /// <summary>
+    /// Can the current user grant this role?
+    /// </summary>
+    public bool CanAccess { get; set; }
+    
+    
+    /// <summary>
+    /// Generate a list of <see cref="StrippedRole"/> from a guild.
+    /// </summary>
+    /// <param name="guild"></param>
+    /// <returns></returns>
+    public static IEnumerable<StrippedRole> FromGuild(DiscordSocketClient client, SocketGuild guild)
+    {
+        var roles = guild.Roles;
+        var currentUserRoles = guild.GetUser(client.CurrentUser.Id).Roles.ToList();
+        var ourHighestRolePosition =
+            currentUserRoles.OrderByDescending(v => v.Position).Select(v => v.Position).Concat(new int[]{int.MinValue})
+                .OrderByDescending(v => v)
+                .FirstOrDefault();
+        var items = new List<StrippedRole>();
+        foreach (var i in roles)
+        {
+            var d = StrippedRole.FromRole(client, i);
+            d.CanAccess = d.Position < ourHighestRolePosition;
+            items.Add(d);
+        }
+
+        return items;
+    }
+
+    public static StrippedRole FromRole(DiscordSocketClient client, SocketRole role)
+    {
+        var instance = new StrippedRole();
+        instance.HexColor = role.Color.ToString() ?? "#00000000";
+        instance.RoleId = role.Id;
+        instance.CreatedAt = role.CreatedAt;
+        instance.Name = role.Name;
+        instance.Position = role.Position;
+        return instance;
+    }
+}
