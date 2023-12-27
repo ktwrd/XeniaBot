2e8361af5821a7d03e674a2d9e0463d4bce391f4
kate <kate@dariox.club>
2023-12-27 15:16:12 +0800

    Add ability to set role rewards

--- XeniaBot.WebPanel/Controllers/ServerSettingsXpController.cs
+++ XeniaBot.WebPanel/Controllers/ServerSettingsXpController.cs
@@ -60,4 +60,72 @@ public partial class ServerController
         }
     }
 
+    [HttpPost("~/Server/{id}/Settings/Xp/RoleGrant/Add")]
+    public async Task<IActionResult> SaveSettings_Xp_RoleGrant_Add(ulong id, string roleId, string requiredLevel)
+    {
+        if (!CanAccess(id))
+            return View("NotAuthorized");
+        
+        var guild = _discord.GetGuild(id);
+        if (guild == null)
+            return View("NotFound", "Guild not found");
+
+        ulong targetRoleId;
+        try
+        {
+            targetRoleId = ulong.Parse(roleId);
+        }
+        catch (Exception ex)
+        {
+            return await Index(id,
+                messageType: "danger",
+                message: $"Failed to add Role Reward item. Failed to parse \"roleId\": {ex.Message}");
+        }
+
+        ulong targetRequiredLevel;
+        try
+        {
+            targetRequiredLevel = ulong.Parse(requiredLevel);
+        }
+        catch (Exception ex)
+        {
+            return await Index(id,
+                messageType: "danger",
+                message: $"Failed to add Role Reward item. Failed to parse \"requiredLevel\": {ex.Message}");
+        }
+
+        try
+        {
+            var controller = Program.Services.GetRequiredService<LevelSystemGuildConfigController>();
+            var data = await controller.Get(guild.Id) ?? new LevelSystemGuildConfigModel()
+            {
+                GuildId = guild.Id,
+            };
+            var roleName = guild.Roles.FirstOrDefault(v => v.Id == targetRoleId);
+            var exists = data.RoleGrant.Any(v => v.RoleId == targetRoleId);
+            if (exists)
+            {
+                return await Index(id,
+                    messageType: "warning",
+                    message: $"Role Reward for \"{roleName?.Name ?? targetRoleId.ToString()}\" already exists.");
+            }
+            data.RoleGrant.Add(new LevelSystemRoleGrantItem()
+            {
+                RoleId = targetRoleId,
+                RequiredLevel = targetRequiredLevel
+            });
+            await controller.Set(data);
+            return await Index(
+                id,
+                messageType: "success",
+                message: $"Added role {roleName?.Name} to Level Up Role Reward");
+        }
+        catch (Exception ex)
+        {
+            Log.Error($"Failed to add item from RoleGrant (guild: {guild.Id}, role: {roleId}, level: {requiredLevel})\n{ex}");
+            return await Index(id,
+                messageType: "danger",
+                message: $"Failed to save Level System settings. {ex.Message}");
+        }
+    }
 }
