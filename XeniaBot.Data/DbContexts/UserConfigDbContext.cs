10535c197a8f3b88b5ea5bc33468238731a592d1
kate <kate@dariox.club>
2024-01-02 07:46:33 +0800

    Add UserConfig DB Context

---
+++ XeniaBot.Data/DbContexts/UserConfigDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MongoDB.Driver;
+using MongoDB.EntityFrameworkCore.Extensions;
+using XeniaBot.Data.Models;
+
+namespace XeniaBot.Data.DbContexts;
+
+public class UserConfigDbContext : DbContext
+{
+    public DbSet<UserConfigModel> Users { get; init; }
+
+    public static UserConfigDbContext Create(IMongoDatabase database)
+    {
+        var opts = new DbContextOptionsBuilder<UserConfigDbContext>()
+            .UseMongoDB(database.Client, database.DatabaseNamespace.DatabaseName)
+            .Options;
+        return new UserConfigDbContext(opts);
+    }
+    
+    public UserConfigDbContext(DbContextOptions options)
+        : base(options)
+    {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<UserConfigModel>().ToCollection(UserConfigModel.CollectionName);
+    }
+}
