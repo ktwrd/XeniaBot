b4705256cf06c832eef51ad885f1d9c8141a1833
kate <kate@dariox.club>
2024-01-02 07:45:59 +0800

    Add interface IBaseController

--- XeniaBot.Shared/_Reflection/BaseController.cs
+++ XeniaBot.Shared/_Reflection/BaseController.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace XeniaBot.Shared
 {
-    public abstract class BaseController
+    public abstract class BaseController : IBaseController
     {
         protected IServiceProvider _services;
         protected BaseController(IServiceProvider services)
@@ -11,22 +11,31 @@ namespace XeniaBot.Shared
             _services = services;
         }
 
-        /// <summary>
-        /// Called when all services have been added to the collection.
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc />
         public virtual Task InitializeAsync()
         {
             return Task.CompletedTask;
         }
 
-        /// <summary>
-        /// Called when discord is ready
-        /// </summary>
-        /// <returns></returns>
+        /// <inheritdoc />
         public virtual Task OnReady()
         {
             return Task.CompletedTask;
         }
+    }
+
+    public interface IBaseController
+    {
+        /// <summary>
+        /// Called when all services have been added to the collection.
+        /// </summary>
+        /// <returns></returns>
+        public Task InitializeAsync();
+
+        /// <summary>
+        /// Called when discord is ready
+        /// </summary>
+        /// <returns></returns>
+        public Task OnReady();
     }
 }
