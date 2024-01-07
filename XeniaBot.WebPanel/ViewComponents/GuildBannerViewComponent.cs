c0c0d94d32d1a2bfdd7201ed3135bb49b6fb28b0
kate <kate@dariox.club>
2024-01-07 14:25:49 +0800

    Create GuildBannerViewComponent

---
+++ XeniaBot.WebPanel/ViewComponents/GuildBannerViewComponent.cs
@@ -0,0 +1,16 @@
+﻿using Discord.WebSocket;
+using Microsoft.AspNetCore.Mvc;
+using XeniaBot.WebPanel.Models;
+
+namespace XeniaBot.WebPanel.ViewComponents;
+
+public class GuildBannerViewComponent : ViewComponent
+{
+    public async Task<IViewComponentResult> InvokeAsync(ulong guildId)
+    {
+        var client = Program.Services.GetRequiredService<DiscordSocketClient>();
+        var guild = client.GetGuild(guildId);
+        var data = StrippedGuild.FromGuild(guild);
+        return View("Default", data);
+    }
+}
