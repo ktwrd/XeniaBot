b77162720b3cbae422e0c6dd83244907cd5bea51
kate <kate@dariox.club>
2024-01-07 13:24:21 +0800

    Move navbar to own component

---
+++ XeniaBot.WebPanel/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace XeniaBot.WebPanel.ViewComponents;
+
+public class NavbarViewComponent : ViewComponent
+{
+    /// <summary>
+    /// Only generate the option elements for a select.
+    /// </summary>
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return View("Default");
+    }
+}
