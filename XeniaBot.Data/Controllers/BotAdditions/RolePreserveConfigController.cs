1456033d0cd819294fc2f379682cde9340fb8217
kate <kate@dariox.club>
2024-01-07 17:00:12 +0800

    Create RolePreserve db stuff

---
+++ XeniaBot.Data/Controllers/BotAdditions/RolePreserveConfigController.cs
@@ -0,0 +1,43 @@
+﻿using MongoDB.Driver;
+using XeniaBot.Data.Models;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Data.Controllers.BotAdditions;
+
+public class RolePreserveConfigController : BaseConfigController<RolePreserveModel>
+{
+    public RolePreserveConfigController(IServiceProvider services)
+        : base(RolePreserveModel.CollectionName, services)
+    { }
+
+
+    public async Task<RolePreserveModel?> Get(ulong userId, ulong guildId)
+    {
+        var collection = GetCollection();
+        var filter = Builders<RolePreserveModel>
+            .Filter
+            .Where(v => v.UserId == userId && v.GuildId == guildId);
+        var res = await collection.FindAsync(filter);
+        return res.FirstOrDefault();
+    }
+
+    public async Task Set(RolePreserveModel model)
+    {
+        var collection = GetCollection();
+        var filter = Builders<RolePreserveModel>
+            .Filter
+            .Where(v => v.UserId == model.UserId && v.GuildId == model.GuildId);
+
+        var existResult = await collection.FindAsync(filter);
+        var exists = existResult.Any();
+
+        if (exists)
+        {
+            await collection.FindOneAndReplaceAsync(filter, model);
+        }
+        else
+        {
+            await collection.InsertOneAsync(model);
+        }
+    }
+}
---
+++ XeniaBot.Data/Models/RolePreserveModel.cs
@@ -0,0 +1,17 @@
+﻿using XeniaBot.Shared.Models;
+
+namespace XeniaBot.Data.Models;
+
+public class RolePreserveModel : BaseModel
+{
+    public static string CollectionName => "rolePreserve";
+
+    public RolePreserveModel()
+        : base()
+    {
+        Roles = new List<ulong>();
+    }
+    public ulong UserId { get; set; }
+    public ulong GuildId { get; set; }
+    public List<ulong> Roles { get; set; }
+}
