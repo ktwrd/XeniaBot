cf9c4d518e473758ff7a9ce476514adafc2c6c4e
kate <kate@dariox.club>
2024-01-25 22:04:58 +0800

    Add function GetInfoAllInGuild

--- XeniaBot.Data/Controllers/BotAdditions/BanSyncInfoConfigController.cs
+++ XeniaBot.Data/Controllers/BotAdditions/BanSyncInfoConfigController.cs
@@ -17,8 +17,15 @@ namespace XeniaBot.Data.Controllers.BotAdditions;
 [BotController]
 public class BanSyncInfoConfigController : BaseConfigController<BanSyncInfoModel>
 {
-    public BanSyncInfoConfigController(IServiceProvider services) : base("banSyncInfo", services)
-    {}
+    private readonly DiscordSocketClient _discord;
+    private readonly BanSyncStateHistoryConfigController _banSyncStateController;
+
+    public BanSyncInfoConfigController(IServiceProvider services)
+        : base("banSyncInfo", services)
+    {
+        _discord = services.GetRequiredService<DiscordSocketClient>();
+        _banSyncStateController = services.GetRequiredService<BanSyncStateHistoryConfigController>();
+    }
     protected async Task<ICollection<BanSyncInfoModel>> BaseInfoFind(FilterDefinition<BanSyncInfoModel> data)
     {
         var collection = GetCollection();
@@ -40,80 +47,119 @@ public class BanSyncInfoConfigController : BaseConfigController<BanSyncInfoModel
 
         return result.FirstOrDefault();
     }
-        #region Get/Set
-        #region Get
-        public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(ulong userId, ulong guildId)
-        {
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Where(v => v.UserId == userId && v.GuildId == guildId);
+    #region Get/Set
+    #region Get
+    public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(ulong userId, ulong guildId)
+    {
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Where(v => v.UserId == userId && v.GuildId == guildId);
 
-            return await BaseInfoFind(filter);
-        }
-        public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(BanSyncInfoModel data)
-            => await GetInfoEnumerable(data.UserId, data.GuildId);
-        public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(ulong userId)
-        {
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Eq("UserId", userId);
+        return await BaseInfoFind(filter);
+    }
+    public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(BanSyncInfoModel data)
+        => await GetInfoEnumerable(data.UserId, data.GuildId);
+    public async Task<ICollection<BanSyncInfoModel>> GetInfoEnumerable(ulong userId)
+    {
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Eq("UserId", userId);
 
-            return await BaseInfoFind(filter);
-        }
-        public async Task<BanSyncInfoModel?> GetInfo(ulong userId, ulong guildId)
-        {
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Where(v => v.UserId == userId && v.GuildId == guildId);
+        return await BaseInfoFind(filter);
+    }
+    public async Task<BanSyncInfoModel?> GetInfo(ulong userId, ulong guildId)
+    {
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Where(v => v.UserId == userId && v.GuildId == guildId);
+        var res = await BaseInfoFind(filter);
+        var sorted = res.OrderByDescending(v => v.Timestamp);
+        return sorted.FirstOrDefault();
+    }
+    public async Task<BanSyncInfoModel?> GetInfo(BanSyncInfoModel data)
+        => await GetInfo(data.UserId, data.GuildId);
 
-            return await BaseInfoFirstOrDefault(filter);
-        }
-        public async Task<BanSyncInfoModel?> GetInfo(BanSyncInfoModel data)
-            => await GetInfo(data.UserId, data.GuildId);
-        #endregion
-
-        public async Task SetInfo(BanSyncInfoModel data)
+    public async Task<List<BanSyncInfoModel>> GetInfoAllInGuild(ulong guildId, bool ignoreDisabledGuilds = true)
+    {
+        var result = new List<BanSyncInfoModel>();
+        var guild = _discord.GetGuild(guildId);
+        foreach (var user in guild.Users)
         {
-            var collection = GetCollection();
             var filter = Builders<BanSyncInfoModel>
                 .Filter
-                .Where(v => v.UserId == data.UserId && v.GuildId == data.GuildId);
-            if (await InfoExists(data))
+                .Where(v => v.UserId == user.Id);
+            var userResult = await BaseInfoFind(filter);
+            var innerUser = new List<BanSyncInfoModel>();
+            foreach (var re in userResult)
             {
-                await collection.ReplaceOneAsync(filter, data);
+                var guildConf = await _banSyncStateController.GetLatest(re.GuildId);
+                if (ignoreDisabledGuilds
+                    && ((guildConf?.Enable ?? false)
+                        || (guildConf?.State ?? BanSyncGuildState.Unknown) == BanSyncGuildState.Active))
+                {
+                    innerUser.Add(re);
+                }
             }
-            else
+
+            if (innerUser.Any())
             {
-                await collection.InsertOneAsync(data);
+                result.Add(innerUser.OrderByDescending(v => v.Timestamp).FirstOrDefault());
             }
         }
-        public async Task RemoveInfo(ulong userId, ulong guildId)
-        {
-            var collection = GetCollection();
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Where(v => v.UserId == userId && v.GuildId == guildId);
 
-            await collection.DeleteManyAsync(filter);
+        foreach (var i in await BaseInfoFind(
+                     Builders<BanSyncInfoModel>
+                         .Filter
+                         .Where(v => v.GuildId == guildId)))
+        {
+            result.Add(i);
         }
-        #endregion
+        return result;
+    }
+    #endregion
 
-        #region Info Exists
-        public async Task<bool> InfoExists(ulong userId, ulong guildId)
+    public async Task SetInfo(BanSyncInfoModel data)
+    {
+        var collection = GetCollection();
+        var filter = Builders<BanSyncInfoModel>
+            .Filter
+            .Where(v => v.UserId == data.UserId && v.GuildId == data.GuildId);
+        if (await InfoExists(data))
         {
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Where(v => v.UserId == userId && v.GuildId == guildId);
-            return await BaseInfoAny(filter);
+            await collection.ReplaceOneAsync(filter, data);
         }
-        public async Task<bool> InfoExists(ulong userId)
+        else
         {
-            var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
-                .Filter
-                .Eq("UserId", userId);
-            return await BaseInfoAny(filter);
+            await collection.InsertOneAsync(data);
         }
-        public async Task<bool> InfoExists(BanSyncInfoModel data)
-            => await InfoExists(data.UserId, data.GuildId);
-        #endregion
+    }
+    public async Task RemoveInfo(ulong userId, ulong guildId)
+    {
+        var collection = GetCollection();
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Where(v => v.UserId == userId && v.GuildId == guildId);
+
+        await collection.DeleteManyAsync(filter);
+    }
+    #endregion
+
+    #region Info Exists
+    public async Task<bool> InfoExists(ulong userId, ulong guildId)
+    {
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Where(v => v.UserId == userId && v.GuildId == guildId);
+        return await BaseInfoAny(filter);
+    }
+    public async Task<bool> InfoExists(ulong userId)
+    {
+        var filter = MongoDB.Driver.Builders<BanSyncInfoModel>
+            .Filter
+            .Eq("UserId", userId);
+        return await BaseInfoAny(filter);
+    }
+    public async Task<bool> InfoExists(BanSyncInfoModel data)
+        => await InfoExists(data.UserId, data.GuildId);
+    #endregion
 }
