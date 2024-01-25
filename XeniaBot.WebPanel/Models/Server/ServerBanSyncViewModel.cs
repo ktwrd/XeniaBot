8f38028705217b8a339567894226b337255944c6
kate <kate@dariox.club>
2024-01-25 22:05:56 +0800

    Add model ServerBanSyncViewModel

---
+++ XeniaBot.WebPanel/Models/Server/ServerBanSyncViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Discord.WebSocket;
+using XeniaBot.Data.Models;
+
+namespace XeniaBot.WebPanel.Models;
+
+public class ServerBanSyncViewModel : BaseViewModel
+{
+    public SocketGuildUser User { get; set; }
+    public SocketGuild Guild { get; set; }
+    public List<BanSyncInfoModel> BanSyncRecords { get; set; }
+}
