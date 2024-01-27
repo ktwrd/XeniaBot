7d964decf0c0d2c37523cf10190ad0262771c225
kate <kate@dariox.club>
2024-01-27 12:25:09 +0800

    Create HealthServer.cs

---
+++ XeniaBot.Shared/HealthServer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Reflection.Metadata;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using XeniaBot.Shared.Controllers;
+using XeniaBot.Shared.Models;
+
+namespace XeniaBot.Shared;
+
+public class HealthServer
+{
+    private readonly CoreContext _core;
+    public HealthServer(CoreContext core)
+    {
+        _core = core;
+    }
+    public void Run(int port)
+    {
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.ConfigureKestrel(
+            serverOptions =>
+            {
+                serverOptions.ListenAnyIP(port);
+            });
+        var app = builder.Build();
+        app.MapGet("/", Handle);
+        app.Run();
+    }
+
+    public string Handle()
+    {
+        var data = new XeniaHealthModel()
+        {
+            StartTimestamp = _core.Details.StartTimestamp,
+            Version = _core.Details.Version,
+            ServiceName = "XeniaDiscordBot"
+        };
+        var json = JsonSerializer.Serialize(data, CoreContext.SerializerOptions);
+        return json;
+    }
+}
