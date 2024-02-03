4e734a13021f3438ce50260defc5adc144fd5447
kate <kate@dariox.club>
2024-02-03 19:42:54 +0800

    Create ReminderServiceConfigItem.cs

---
+++ XeniaBot.Shared/_Config/ReminderServiceConfigItem.cs
@@ -0,0 +1,17 @@
+﻿namespace XeniaBot.Shared;
+
+public class ReminderServiceConfigItem
+{
+    public bool Enable { get; set; }
+
+    public static ReminderServiceConfigItem Default(ReminderServiceConfigItem? i = null)
+    {
+        i ??= new ReminderServiceConfigItem();
+        i.Enable = false;
+        return i;
+    }
+    public ReminderServiceConfigItem()
+    {
+        Default(this);
+    }
+}
