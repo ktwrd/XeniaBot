43da54fc6cbf5a36be66d25921c0f1c79b88d40d
kate <kate@dariox.club>
2024-02-18 12:30:04 +0800

    Add ability to refresh weather messages

--- XeniaBot.Core/Modules/WeatherModule.cs
+++ XeniaBot.Core/Modules/WeatherModule.cs
@@ -11,6 +11,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using XeniaBot.Core.Services.BotAdditions;
+using XeniaBot.Shared;
+using XeniaBot.Shared.Services;
 
 namespace XeniaBot.Core.Modules
 {
@@ -22,8 +25,36 @@ namespace XeniaBot.Core.Modules
             [Summary("system", description: "Measurement system to fetch the weather in.")]
             MeasurementSystem syst)
         {
-            var resultEmbed = await WeatherEmbedHelper.CurrentForecast(location, syst);
-            await Context.Interaction.RespondAsync(embed: resultEmbed.Build());
+            await DeferAsync();
+            var controller = CoreContext.Instance?.GetRequiredService<WeatherModuleService>();
+            if (controller == null)
+            {
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                        .WithTitle("Weather - Today")
+                        .WithDescription($"Failed to get WeatherModuleService")
+                        .WithColor(Color.Red)
+                        .Build());
+                return;
+            }
+
+            try
+            {
+                var resultEmbed = await controller.GetCurrentWeatherEmbed(location, syst);
+                await Context.Interaction.FollowupAsync(embed: resultEmbed.Build(),
+                    components: controller.WeatherCurrentComponents(location, syst).Build());
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failed to fetch weather details for {location}.\n{ex}");
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                        .WithTitle("Weather - Today")
+                        .WithDescription($"Failed to fetch data. \n```\n{ex.Message}\n```")
+                        .WithColor(Color.Red)
+                        .Build());
+                await CoreContext.Instance?.GetRequiredService<ErrorReportService>()?.ReportError(ex, Context);
+            }
         }
 
         [SlashCommand("forecast", "Fetch 3 day weather forecast")]
@@ -31,8 +62,36 @@ namespace XeniaBot.Core.Modules
             [Summary("system", description: "Measurement system to fetch the weather in.")]
             MeasurementSystem syst)
         {
-            var resultEmbed = await WeatherEmbedHelper.Forecast(location, syst);
-            await Context.Interaction.RespondAsync(embed: resultEmbed.Build());
+            await DeferAsync();
+            var controller = CoreContext.Instance?.GetRequiredService<WeatherModuleService>();
+            if (controller == null)
+            {
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                        .WithTitle("Weather - Forecast")
+                        .WithDescription($"Failed to get WeatherModuleService")
+                        .WithColor(Color.Red)
+                        .Build());
+                return;
+            }
+
+            try
+            {
+                var resultEmbed = await controller.GetForecastEmbed(location, syst);
+                await Context.Interaction.FollowupAsync(embed: resultEmbed.Build(),
+                    components: controller.WeatherForecastComponents(location, syst).Build());
+            }
+            catch (Exception ex)
+            {
+                Log.Error($"Failed to fetch weather forecast for {location}.\n{ex}");
+                await FollowupAsync(
+                    embed: new EmbedBuilder()
+                        .WithTitle("Weather - Forecast")
+                        .WithDescription($"Failed to fetch data. \n```\n{ex.Message}\n```")
+                        .WithColor(Color.Red)
+                        .Build());
+                await CoreContext.Instance?.GetRequiredService<ErrorReportService>()?.ReportError(ex, Context);
+            }
         }
     }
 }
