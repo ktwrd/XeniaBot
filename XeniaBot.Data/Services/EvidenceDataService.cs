28bac678462fca6555817e556d2b38cc72fbbd01
kate <kate@dariox.club>
2024-02-19 21:18:29 +0800

    Create EvidenceDataService.cs

---
+++ XeniaBot.Data/Services/EvidenceDataService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Data.Services;
+
+[XeniaController]
+public class EvidenceDataService : BaseService
+{
+    private readonly ConfigData _configData;
+    public EvidenceDataService(IServiceProvider services)
+        : base(services)
+    {
+        _configData = services.GetRequiredService<ConfigData>();
+    }
+}
