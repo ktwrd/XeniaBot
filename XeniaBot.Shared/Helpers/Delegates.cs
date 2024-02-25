af053379d37b9628e3ab156d096331c08de79437
kate <kate@dariox.club>
2024-02-25 15:21:15 +0800

    Update Delegates.cs

--- XeniaBot.Shared/Helpers/Delegates.cs
+++ XeniaBot.Shared/Helpers/Delegates.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,4 +10,8 @@ namespace XeniaBot.Shared.Helpers
 {
     public delegate void DiscordControllerDelegate(DiscordService service);
     public delegate Task TaskDelegate();
+
+    public delegate void LockStateItemDelegate<T, TH>(
+        [DisallowNull] T key,
+        [DisallowNull]TH value);
 }
