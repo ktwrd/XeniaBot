9bccd0d7c58bc8c9d5a1ad9c58bce269d4718616
kate <kate@dariox.club>
2024-02-25 16:07:34 +0800

    Create Delegates.cs

---
+++ XeniaBot.Moderation/Helpers/Delegates.cs
@@ -0,0 +1,5 @@
+﻿using Discord.WebSocket;
+
+namespace XeniaBot.Moderation.Helpers;
+
+public delegate Task UserKickedDelegate(SocketGuild guildId, ulong userId, ulong actionedByUserId, string? reason, DateTimeOffset timestamp);
