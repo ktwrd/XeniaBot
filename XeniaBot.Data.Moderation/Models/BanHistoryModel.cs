28bcf3955f0a2ad82496306a2e552f6d350b913f
kate <kate@dariox.club>
2024-02-25 16:08:07 +0800

    Add collection mod_banRecord_history

---
+++ XeniaBot.Data.Moderation/Models/BanHistoryModel.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace XeniaBot.Data.Moderation.Models
+{
+    public class BanHistoryModel
+    {
+        public static string CollectionName => "mod_banRecord_history";
+        [BsonElement("_id")]
+        public Guid Id { get; set; }
+        public ulong UserId { get; set; }
+        public ulong GuildId { get; set; }
+        public bool IsBanned { get; set; }
+        /// <summary>
+        /// When this person was banned. Seconds since UTC Epoch.
+        /// </summary>
+        public long Timestamp { get; set; }
+        /// <summary>
+        /// When not `null`, it is set to <see cref="BanRecordModel.Id"/> because it is a many-to-one relationship (many is <see cref="BanHistoryModel"/>).
+        /// </summary>
+        public Guid? BanRecordId { get; set; }
+        public string? Reason { get; set; }
+
+        public BanHistoryModel()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
---
+++ XeniaBot.Data.Moderation/Repositories/BanHistoryRepository.cs
@@ -0,0 +1,106 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using XeniaBot.Data.Moderation.Models;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Data.Moderation.Repositories;
+
+[XeniaController]
+public class BanHistoryRepository : BaseRepository<BanHistoryModel>
+{
+    public BanHistoryRepository(IServiceProvider services)
+        : base(BanHistoryModel.CollectionName, services)
+    { }
+
+    private SortDefinition<BanHistoryModel> BaseSort()
+    {
+        return Builders<BanHistoryModel>.Sort.Descending(v => v.Timestamp)!;
+    }
+
+    public async Task<BanHistoryModel?> GetLatest(ulong guildId, ulong userId)
+    {
+        var filter = Builders<BanHistoryModel>
+            .Filter
+            .Where(v => v.GuildId == guildId && v.UserId == userId);
+        var res = await BaseFind(filter, BaseSort());
+        return res.FirstOrDefault();
+    }
+
+    public async Task<List<BanHistoryModel>?> GetMany(ulong guildId, ulong userId)
+    {
+        var filter = Builders<BanHistoryModel>
+            .Filter
+            .Where(v => v.GuildId == guildId && v.UserId == userId);
+        var res = await BaseFind(filter, BaseSort());
+        return res.ToList();
+    }
+
+    /// <summary>
+    /// Get all ban records for user. Will be sorted by <see cref="BanHistoryModel.Timestamp"/> in descending order.
+    /// </summary>
+    /// <param name="userId">User Id to fetch the ban records for.</param>
+    public async Task<List<BanHistoryModel>?> GetManyUser(ulong userId)
+    {
+        var filter = Builders<BanHistoryModel>
+            .Filter
+            .Where(v => v.UserId == userId);
+        var res = await BaseFind(filter, BaseSort());
+        return res.ToList();
+    }
+    /// <summary>
+    /// Get all bans in guild. Will be sorted by <see cref="BanHistoryModel.Timestamp"/> in descending order.
+    /// </summary>
+    /// <param name="guildId">Guild Id to filter the bans by.</param>
+    public async Task<List<BanHistoryModel>?> GetManyInGuild(ulong guildId)
+    {
+        var filter = Builders<BanHistoryModel>
+            .Filter
+            .Where(v => v.GuildId == guildId);
+        var res = await BaseFind(filter, BaseSort());
+        return res.ToList();
+    }
+
+    /// <summary>
+    /// Converts a list of records into a dict, which is mapped via the User Id.
+    /// </summary>
+    public Dictionary<ulong, List<BanHistoryModel>> MapKeyToUser(List<BanHistoryModel> items)
+    {
+        var dict = new Dictionary<ulong, List<BanHistoryModel>>();
+        items = items.OrderByDescending(v => v.Timestamp).ToList();
+        foreach (var item in items)
+        {
+            if (!dict.ContainsKey(item.UserId))
+                dict.Add(item.UserId, new List<BanHistoryModel>());
+
+            dict[item.UserId].Add(item);
+        }
+
+        return dict;
+    }
+
+    /// <summary>
+    /// Insert or Update a record based off <see cref="BanHistoryModel.Id"/>
+    /// </summary>
+    /// <param name="model">Model to replace with.</param>
+    public async Task<BanHistoryModel> InsertOrUpdate(BanHistoryModel model)
+    {
+        var collection = GetCollection();
+        var filter = Builders<BanHistoryModel>.Filter.Where(v => v.Id == model.Id);
+        model.Timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        var anyResult = await collection.FindAsync(filter);
+        if (anyResult.Any())
+        {
+            await collection.ReplaceOneAsync(filter, model);
+        }
+        else
+        {
+            await collection.InsertOneAsync(model);
+        }
+
+        return model;
+    }
+}
