e61279b3de21be5922571fefe8dc4837235c5d6e
kate <kate@dariox.club>
2024-02-25 16:08:16 +0800

    Add collection mod_auditLogCheck

---
+++ XeniaBot.Data.Moderation/Models/AuditLogCheckRecord.cs
@@ -0,0 +1,22 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace XeniaBot.Data.Moderation.Models;
+
+public class AuditLogCheckRecord
+{
+    public static string CollectionName => "mod_auditLogCheck";
+    [BsonElement("id")]
+    public Guid Id { get; set; }
+
+    public ulong GuildId { get; set; }
+    public string ActionType { get; set; }
+    public long Timestamp { get; set; }
+    public long InsertTimestamp { get; set; }
+    public ulong? LastId { get; set; }
+
+    public AuditLogCheckRecord()
+    {
+        Id = Guid.NewGuid();
+    }
+
+}
---
+++ XeniaBot.Data.Moderation/Repositories/AuditLogCheckRepository.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+using XeniaBot.Data.Moderation.Models;
+using XeniaBot.Shared;
+
+namespace XeniaBot.Data.Moderation.Repositories;
+
+[XeniaController]
+public class AuditLogCheckRepository : BaseRepository<AuditLogCheckRecord>
+{
+    public AuditLogCheckRepository(IServiceProvider services)
+        : base(AuditLogCheckRecord.CollectionName, services)
+    { }
+
+    public async Task<AuditLogCheckRecord?> Get(ulong guildId, string actionType)
+    {
+        var filter = Builders<AuditLogCheckRecord>
+            .Filter
+            .Where(v => v.GuildId == guildId && v.ActionType == actionType);
+        var sort = Builders<AuditLogCheckRecord>
+            .Sort
+            .Descending(v => v.Timestamp);
+        var res = await BaseFind(filter, sort);
+        return res.FirstOrDefault();
+    }
+
+    public async Task<AuditLogCheckRecord> Add(AuditLogCheckRecord model)
+    {
+        model.InsertTimestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
+        var collection = GetCollection();
+        await collection.InsertOneAsync(model);
+        return model;
+    }
+}
