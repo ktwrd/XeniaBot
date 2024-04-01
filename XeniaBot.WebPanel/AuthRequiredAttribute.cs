e82252afeeec3ce7b0f4d3d62bd2a79b2d874ed4
kate <kate@dariox.club>
2024-04-01 09:47:06 +0800

    Display instead of Redirect for AuthRequiredAttribute

--- XeniaBot.WebPanel/AuthRequiredAttribute.cs
+++ XeniaBot.WebPanel/AuthRequiredAttribute.cs
@@ -1,58 +1,141 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 using XeniaBot.WebPanel.Helpers;
 using XeniaBot.WebPanel.Models;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using XeniaBot.Shared;
+using XeniaBot.Shared.Services;
 
 namespace XeniaBot.WebPanel;
 
+/// <summary>
+/// Restrict usage of the Route for only authenticated users.
+///
+/// <para>Can be configured to only allow specified Guild (<see cref="GuildIdRouteDataName"/>) or if the user must be a bot owner (<see cref="RequireWhitelist"/>)</para>
+/// </summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
 public class AuthRequiredAttribute : ActionFilterAttribute
 {
-    public bool ShowLoginButton { get; set; }
+    /// <summary>
+    /// <para>Optional: Requesting User Id must be in the provided guild and must have the Manage Server permission.</para>
+    /// </summary>
+    [DefaultValue(null)]
     public ulong? RequireGuildId { get; set; }
+    /// <summary>
+    /// <para>Route/Parameter name where the Guild Id is set.</para>
+    ///
+    /// <para>Requesting User must have the Manage Server permission</para>
+    ///
+    /// <para><b>Overrides <see cref="RequireGuildId"/></b></para>
+    /// </summary>
+    [DefaultValue(null)]
+    public string? GuildIdRouteDataName { get; set; }
+    /// <summary>
+    /// Require the requesting user to be in <see cref="ConfigData.UserWhitelist"/>. Used for bot owner-only sections.
+    /// </summary>
+    [DefaultValue(false)]
+    public bool RequireWhitelist { get; set; }
     public override void OnActionExecuting(ActionExecutingContext context)
     {
+        // Only allow authenticated users.
         var isAuth = context.HttpContext.User?.Identity?.IsAuthenticated ?? false;
-        if (isAuth)
+        if (!isAuth)
         {
+            context.Result = new ViewResult
+            {
+                ViewName = "NotAuthorized",
+                ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), context.ModelState)
+                {
+                    Model = new NotAuthorizedViewModel()
+                    {
+                        ShowLoginButton = true
+                    }
+                }
+            };
+            return;
+        }
+
+        if (RequireWhitelist)
+        {
+            var userId = AspHelper.GetUserId(context.HttpContext) ?? 0;
+            if (!CoreContext.Instance?.GetRequiredService<ConfigData>().UserWhitelist.Contains((ulong)userId) ?? false)
+            {
+                context.Result = new ViewResult
+                {
+                    ViewName = "NotAuthorized"
+                };
+                return;
+            }
             base.OnActionExecuting(context);
             return;
         }
 
-        if (RequireGuildId != null)
+        ulong? targetGuildId = RequireGuildId;
+        if (GuildIdRouteDataName != null)
         {
-            var userId = AspHelper.GetUserId(context.HttpContext) ?? 0;
-            ulong guildId = (ulong)RequireGuildId;
-            bool canAccess = AspHelper.CanAccessGuild(guildId, userId);
-            if (canAccess)
+            if (context.RouteData.Values.TryGetValue(GuildIdRouteDataName, out var s))
             {
-                base.OnActionExecuting(context);
+                try
+                {
+                    targetGuildId = ulong.Parse(s?.ToString() ?? "0");
+                    if (targetGuildId == null || targetGuildId < 1)
+                        throw new Exception();
+                }
+                catch
+                {
+                    targetGuildId = null;
+                }
+            }
+
+            if (targetGuildId == null)
+            {
+                context.Result = new ViewResult
+                {
+                    ViewName = "NotAuthorized",
+                    ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), context.ModelState)
+                    {
+                        Model = new NotAuthorizedViewModel()
+                        {
+                            Message = "Invalid Guild Provided"
+                        }
+                    }
+                };
                 return;
             }
         }
-        var data = new NotAuthorizedViewModel
+        if (targetGuildId != null)
         {
-            ShowLoginButton = ShowLoginButton
-        };
-        /*var view = new Views_Shared_NotAuthorized();
-        view.ViewData.Model = data;
-        var result = new ViewResult
-        {
-            ViewName = "NotAuthorized",
-            ViewData = view.ViewData
-        };*/
-        var result = new RedirectToActionResult("NotAuth", "Root", data)
-        {
-            PreserveMethod = false,
-        };
-        context.Result = result;
+            var userId = AspHelper.GetUserId(context.HttpContext) ?? 0;
+            bool canAccess = AspHelper.CanAccessGuild((ulong)targetGuildId!, userId);
+            if (!canAccess)
+            {
+                context.Result = new ViewResult
+                {
+                    ViewName = "NotAuthorized",
+                    ViewData = new ViewDataDictionary(new EmptyModelMetadataProvider(), context.ModelState)
+                    {
+                        Model = new NotAuthorizedViewModel()
+                        {
+                            Message = "Missing permission Manage Guild"
+                        }
+                    }
+                };
+                return;
+            }
+        }
+        base.OnActionExecuting(context);
     }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AuthRequiredAttribute"/> class.
+    /// </summary>
     public AuthRequiredAttribute()
     {
-        ShowLoginButton = false;
+        RequireWhitelist = false;
     }
 }
