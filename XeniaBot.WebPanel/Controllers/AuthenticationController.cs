ed5c62e28b2b002f6cbd99a79e0716b204a16848
kate <kate@dariox.club>
2024-04-01 09:46:57 +0800

    Add method SignInDiscord

--- XeniaBot.WebPanel/Controllers/AuthenticationController.cs
+++ XeniaBot.WebPanel/Controllers/AuthenticationController.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
@@ -12,7 +13,7 @@ public class AuthenticationController : Controller
     public async Task<IActionResult> SignIn() => View("SignIn", await HttpContext.GetExternalProvidersAsync());
 
     [HttpPost("~/signin")]
-    public async Task<IActionResult> SignIn([FromForm] string provider)
+    public async Task<IActionResult> SignIn([FromForm] string provider, string redirect = "/")
     {
         // Note: the "provider" parameter corresponds to the external
         // authentication provider choosen by the user agent.
@@ -26,10 +27,38 @@ public class AuthenticationController : Controller
             return BadRequest();
         }
 
+        var parsedUri = new Uri("https://xb.kate.pet" + redirect);
+        redirect = parsedUri.PathAndQuery;
+
         // Instruct the middleware corresponding to the requested external identity
         // provider to redirect the user agent to its own authorization endpoint.
         // Note: the authenticationScheme parameter must match the value configured in Startup.cs
-        return Challenge(new AuthenticationProperties { RedirectUri = "/" }, provider);
+        return Challenge(new AuthenticationProperties { RedirectUri = redirect }, provider);
+    }
+
+    [HttpGet("~/signin/discord")]
+    public async Task<IActionResult> SignInDiscord(string redirect = "/")
+    {
+        string provider = "Discord";
+        // Note: the "provider" parameter corresponds to the external
+        // authentication provider choosen by the user agent.
+        if (string.IsNullOrWhiteSpace(provider))
+        {
+            return BadRequest();
+        }
+
+        if (!await HttpContext.IsProviderSupportedAsync(provider))
+        {
+            return BadRequest();
+        }
+
+        var parsedUri = new Uri("https://xb.kate.pet" + redirect);
+        redirect = parsedUri.PathAndQuery;
+
+        // Instruct the middleware corresponding to the requested external identity
+        // provider to redirect the user agent to its own authorization endpoint.
+        // Note: the authenticationScheme parameter must match the value configured in Startup.cs
+        return Challenge(new AuthenticationProperties { RedirectUri = redirect }, provider);
     }
 
     [HttpGet("~/signout")]
