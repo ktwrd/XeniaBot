2431d31fb582f3126b92d116f6866f2d4e2ac8e4
kate <kate@dariox.club>
2024-04-06 10:35:16 +0800

    Add infinite scrolling for admin server list (with HTMX!)

--- XeniaBot.WebPanel/Controllers/Admin/Index.cs
+++ XeniaBot.WebPanel/Controllers/Admin/Index.cs
@@ -1,6 +1,12 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord.WebSocket;
 using Microsoft.AspNetCore.Mvc;
+using XeniaBot.WebPanel.Helpers;
 using XeniaBot.WebPanel.Models;
+using XeniaBot.WebPanel.Models.Component;
 
 namespace XeniaBot.WebPanel.Controllers;
 
@@ -17,5 +23,24 @@ public partial class AdminController
         };
         await PopulateModel(model);
         return View(model);
+    }
+
+    [HttpGet("~/Admin/Components/ServerList")]
+    [AuthRequired]
+    [RequireSuperuser]
+    public IActionResult ServerListComponent(int cursor = 1)
+    {
+        var model = new AdminServerListViewModel
+        {
+            Guilds = PaginateGuild(_client.Guilds, cursor, 3),
+            Cursor = cursor
+        };
+        return PartialView("ServerListComponent", model);
+    }
+
+
+    public List<SocketGuild> PaginateGuild(IEnumerable<SocketGuild> data, int page, int pageSize = 10)
+    {
+        return AspHelper.Paginate<SocketGuild, string>(data, v => v.Name, page, pageSize);
     }
 }
---
+++ XeniaBot.WebPanel/Models/Admin/Component/AdminServerListViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Discord.WebSocket;
+
+namespace XeniaBot.WebPanel.Models.Component;
+
+public class AdminServerListViewModel
+{
+    public IEnumerable<SocketGuild> Guilds { get; set; }
+    public int Cursor { get; set; }
+    public bool IsLast => Guilds.Count() < 3;
+
+    public bool IsGuildLast(SocketGuild guild)
+    {
+        if (Guilds.Count() < 2)
+            return true;
+
+        return Guilds.ElementAt(Guilds.Count() - 1).Id == guild.Id;
+    }
+}
