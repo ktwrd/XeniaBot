28cd712b6d8cab4ced470474fe8608b17d31666d
kate <kate@dariox.club>
2024-04-07 19:26:24 +0800

    Implement BasePagination in AdminServerListViewModel

--- XeniaBot.WebPanel/Helpers/AspHelper.cs
+++ XeniaBot.WebPanel/Helpers/AspHelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
@@ -259,7 +260,15 @@ public static class AspHelper
 
     public static List<TSource> Paginate<TSource, TKey>(IEnumerable<TSource> data, Func<TSource, TKey> keySelector, int page = 1, int pageSize = 10)
     {
-        return data.OrderBy(keySelector)
+        return Paginate(data, v => v.OrderBy(keySelector), page, pageSize);
+    }
+
+    public static List<TSource> Paginate<TSource>(IEnumerable<TSource> data,
+        Func<IEnumerable<TSource>, IEnumerable<TSource>> logic,
+        int page = 1,
+        int pageSize = 10)
+    {
+        return logic(data)
             .Skip((page - 1) * pageSize)
             .Take(pageSize)
             .ToList();
