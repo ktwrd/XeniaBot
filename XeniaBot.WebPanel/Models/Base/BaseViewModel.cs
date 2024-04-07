9a19c3007990a815bf0ab92aef4eacefa44c6e84
kate <kate@dariox.club>
2024-04-07 17:52:30 +0800

    Implement HTMX to Preferences

--- XeniaBot.WebPanel/Models/Base/BaseViewModel.cs
+++ XeniaBot.WebPanel/Models/Base/BaseViewModel.cs
@@ -3,7 +3,7 @@ using XeniaBot.Data.Models;
 
 namespace XeniaBot.WebPanel.Models;
 
-public class BaseViewModel
+public class BaseViewModel : IAlertViewModel
 {
     public DiscordSocketClient Client { get; set; }
     public UserConfigModel UserConfig { get; set; }
