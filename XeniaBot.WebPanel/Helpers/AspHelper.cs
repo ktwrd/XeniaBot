f8d0e73d7abdcb01a7a6e4a1dd078ab28140506b
kate <kate@dariox.club>
2024-04-13 19:56:48 +0800

    Update AspHelper.cs

--- XeniaBot.WebPanel/Helpers/AspHelper.cs
+++ XeniaBot.WebPanel/Helpers/AspHelper.cs
@@ -212,11 +212,21 @@ public static class AspHelper
         return data;
     }
 
-    public static DateTime DateTimeFromTimestamp(long timestamp)
+    public static DateTime DateTimeFromTimestamp(long timestamp, bool seconds = false)
     {
         // Unix timestamp is seconds past epoch
         DateTime dateTime = new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc);
-        dateTime = dateTime.AddMilliseconds( timestamp ).ToLocalTime();
+        if (timestamp < 1)
+            return dateTime;
+        Log.Debug(timestamp);
+        if (seconds)
+        {
+            dateTime = dateTime.AddSeconds(timestamp).ToLocalTime();
+        }
+        else
+        {
+            dateTime = dateTime.AddMilliseconds(timestamp).ToLocalTime();
+        }
         return dateTime;
     }
 
