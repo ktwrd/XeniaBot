c6028a8a613b430c1ac875e9e6c7ae4719f1e8aa
kate <kate@dariox.club>
2024-04-14 13:34:18 +0800

    Add interface IBaseViewModel

--- XeniaBot.WebPanel/Models/Base/BaseViewModel.cs
+++ XeniaBot.WebPanel/Models/Base/BaseViewModel.cs
@@ -3,11 +3,17 @@ using XeniaBot.Data.Models;
 
 namespace XeniaBot.WebPanel.Models;
 
-public class BaseViewModel : IAlertViewModel
+public class BaseViewModel : IBaseViewModel
 {
     public DiscordSocketClient Client { get; set; }
     public UserConfigModel UserConfig { get; set; }
     
     public string? MessageType { get; set; }
     public string? Message { get; set; }
+}
+
+public interface IBaseViewModel : IAlertViewModel
+{
+    public DiscordSocketClient Client { get; set; }
+    public UserConfigModel UserConfig { get; set; }
 }
