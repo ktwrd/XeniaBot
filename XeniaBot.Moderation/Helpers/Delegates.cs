706e29b6847375a0ced5f22be8a074625a7f94b9
kate <kate@dariox.club>
2024-05-02 12:35:48 +0800

    Add delegate MemberBannedDelegate

--- XeniaBot.Moderation/Helpers/Delegates.cs
+++ XeniaBot.Moderation/Helpers/Delegates.cs
@@ -6,7 +6,22 @@ namespace XeniaBot.Moderation.Helpers;
 /// <summary>
 /// Used when notifying that a member was kicked.
 /// </summary>
-public delegate Task MemberKickedDelegate(SocketGuild guildId, ulong userId, ulong actionedByUserId, string? reason, DateTimeOffset timestamp);
+public delegate Task MemberKickedDelegate(
+    SocketGuild guild,
+    ulong userId,
+    ulong actionedByUserId,
+    string? reason,
+    DateTimeOffset timestamp);
+
+/// <summary>
+/// Used for when notifying that a member was banned.
+/// </summary>
+public delegate Task MemberBannedDelegate(
+    SocketGuild guild,
+    ulong userId,
+    ulong actionedByUserId,
+    string? reason,
+    DateTimeOffset timestamp);
 
 /// <summary>
 /// Used when notifying that a member was banned.
