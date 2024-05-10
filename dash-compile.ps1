e2ad9af0ba76d34fe4d9c0faad89e8de42663d68
kate <kate@dariox.club>
2024-05-10 21:00:30 +0800

    Update dash-compile.ps1

--- dash-compile.ps1
+++ dash-compile.ps1
@@ -1,21 +1,21 @@
 param(
 	[string]
-	$TargetName="ktwrd/xenia-discord-dash",
+	$TargetName="ktwrd/xenia-discord-webpanel",
 	[string]
 	$TargetTag="latest",
 	[bool]
 	$IncludeTimeTag=1
 )
 $targetTag="$($TargetName):$($TargetTag)"
-docker build -t xenia-discord-dash:latest -f WebPanel.Dockerfile .
+docker build -t xenia-webpanel:latest -f WebPanel.Dockerfile .
 
-docker tag xenia-discord-dash:latest $targetTag
+docker tag xenia-webpanel:latest $targetTag
 docker push $targetTag
 
 if ($IncludeTimeTag -eq $True)
 {
 	$currentTimeTag=@(Get-Date -UFormat %s -Millisecond 0)
 	$timeTag="$($TargetName):$($currentTimeTag)"
-	docker tag xenia-discord-dash:latest $timeTag
+	docker tag xenia-webpanel:latest $timeTag
 	docker push $timeTag
 }
