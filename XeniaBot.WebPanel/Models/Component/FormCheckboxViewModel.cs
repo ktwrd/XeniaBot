20e7a7ded6f75181a267f619dc3acec6908adede
kate <kate@dariox.club>
2024-08-15 19:14:24 +0800

    [FormCheckboxViewModel] Calculate div classes in ViewModel

--- XeniaBot.WebPanel/Models/Component/FormCheckboxViewModel.cs
+++ XeniaBot.WebPanel/Models/Component/FormCheckboxViewModel.cs
@@ -8,5 +8,21 @@ public class FormCheckboxViewModel
     public string Label { get; set; }
     public bool State { get; set; }
     public string? ExtraClasses { get; set; }
+    public string CalculatedClasses
+    {
+        get
+        {
+            string s = "form-check";
+            if (Margin)
+            {
+                s += " mb-3";
+            }
+            if (string.IsNullOrEmpty(ExtraClasses) == false)
+            {
+                s += " " + ExtraClasses;
+            }
+            return s;
+        }
+    }
     public bool Margin { get; set; } = false;
 }
