acbd223a0121e8d6275e89693ea90ee797cf8a08
kate <kate@dariox.club>
2024-10-01 21:41:44 +0800

    [XeniaHelper] Add methods to use reflection to serialize one level of an object into a dictionary w/ reflection

--- XeniaBot.Shared/Helpers/XeniaHelper.cs
+++ XeniaBot.Shared/Helpers/XeniaHelper.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -199,5 +202,173 @@ public static class XeniaHelper
         str += hex;
         var color = System.Drawing.ColorTranslator.FromHtml(str);
         return new Discord.Color(color.R, color.G, color.B);
+    }
+
+    public static Dictionary<string, object?>? ReflectionToDictionary(object? obj, out List<string> skippedProperties)
+    {
+        skippedProperties = new();
+        if (obj == null)
+            return null;
+        var dict = new Dictionary<string, object?>();
+        var properties = obj.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        var allowedTypes = new List<Type>()
+        {
+            typeof(string),
+            typeof(int),
+            typeof(uint),
+            typeof(long),
+            typeof(ulong),
+            typeof(byte),
+            typeof(char)
+        };
+        foreach (var x in properties)
+        {
+            bool found = false;
+            foreach (var it in allowedTypes)
+            {
+                if (it.IsAssignableFrom(x.PropertyType))
+                {
+                    // only allow enums to be casted into non-strings.
+                    if (x.PropertyType.IsEnum &&
+                        (typeof(string).IsAssignableFrom(x.PropertyType) ||
+                         typeof(char).IsAssignableFrom(x.PropertyType)))
+                        continue;
+                    dict[x.Name] = x.GetValue(obj)?.ToString();
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+            {
+                skippedProperties.Add(x.Name);
+            }
+        }
+
+        return dict;
+    }
+    public static Dictionary<string, object?>? DictionarySerialize(this SocketMessage? message)
+    {
+        var dict = ReflectionToDictionary(message, out var skippedProperties);
+        if (dict == null || message == null)
+            return null;
+        var properties = message.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var x in properties.Where(v => skippedProperties.Contains(v.Name)))
+        {
+            if (typeof(IGuild).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IGuild?)x.GetValue(message);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(ICategoryChannel).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (ICategoryChannel?)x.GetValue(message);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(IEmote).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IEmote?)x.GetValue(message);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+        }
+
+        return dict;
+    }
+
+    public static Dictionary<string, object?>? DictionarySerialize(this ISocketMessageChannel? channel)
+    {
+        var dict = ReflectionToDictionary(channel, out var skippedProperties);
+        if (dict == null || channel == null)
+            return null;
+        var properties = channel.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var x in properties.Where(v => skippedProperties.Contains(v.Name)))
+        {
+            if (typeof(IGuild).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IGuild?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(ICategoryChannel).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (ICategoryChannel?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(IEmote).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IEmote?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+        }
+
+        return dict;
+    }
+
+    public static Dictionary<string, object?>? DictionarySerialize(this SocketForumChannel? channel)
+    {
+        var dict = ReflectionToDictionary(channel, out var skippedProperties);
+        if (dict == null || channel == null)
+            return null;
+        var properties = channel.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var x in properties.Where(v => skippedProperties.Contains(v.Name)))
+        {
+            if (typeof(IGuild).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IGuild?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(ICategoryChannel).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (ICategoryChannel?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(IEmote).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IEmote?)x.GetValue(channel);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+        }
+        return dict;
+    }
+
+    public static Dictionary<string, object?>? DictionarySerialize(this IGuild? guild)
+    {
+        var dict = ReflectionToDictionary(guild, out var skippedProperties);
+        if (dict == null || guild == null)
+            return null;
+        var properties = guild.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        foreach (var x in properties.Where(v => skippedProperties.Contains(v.Name)))
+        {if (typeof(ICategoryChannel).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (ICategoryChannel?)x.GetValue(guild);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(IEmote).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IEmote?)x.GetValue(guild);
+                dict[x.Name] = ReflectionToDictionary(obj, out var _);
+            }
+            else if (typeof(ReadOnlyCollection<SocketCustomSticker>).IsAssignableFrom(x.PropertyType) || typeof(IEnumerable<SocketCustomSticker>).IsAssignableFrom(x.PropertyType))
+            {
+                var obj = (IEnumerable<SocketCustomSticker>?)x.GetValue(guild);
+                dict[x.Name] = obj.DictionarySerialize();
+            }
+        }
+        return dict;
+    }
+
+    public static List<Dictionary<string, object?>>? DictionarySerialize(
+        this IEnumerable<ICustomSticker>? stickers)
+    {
+        if (stickers == null)
+            return null;
+        var result = new List<Dictionary<string, object?>>();
+        foreach (var x in stickers)
+        {
+            var v = ReflectionToDictionary(x, out var _);
+            if (v != null)
+                result.Add(v);
+        }
+
+        return result;
     }
 }
