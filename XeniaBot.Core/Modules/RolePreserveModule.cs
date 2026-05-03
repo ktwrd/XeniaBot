20a35c41677bfbf37fabc9f8f0e3182486439ea4
kate <kate@dariox.club>
2026-05-03 14:07:13 +0800

    RolePreserveModule: Implement commands to add/remove role from blacklist & implemented embed generator for listing blacklisted roles

--- XeniaBot.Core/Modules/RolePreserveModule.cs
+++ XeniaBot.Core/Modules/RolePreserveModule.cs
@@ -2,10 +2,15 @@
 using Discord.Interactions;
 using Microsoft.Extensions.DependencyInjection;
 using System;
+using System.Linq;
 using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 using NLog;
 using XeniaBot.Core.Helpers;
 using XeniaBot.Shared;
+using XeniaBot.Shared.Helpers;
 using XeniaBot.Shared.Services;
 using XeniaDiscord.Data;
 using XeniaDiscord.Data.Repositories;
@@ -94,6 +99,8 @@ public class RolePreserveModule : InteractionModuleBase
     }
 
     [SlashCommand("ignore", "Add/Remove/List Ignored Roles")]
+    [RequireUserPermission(GuildPermission.ManageRoles)]
+    [RegisterDBLCommand]
     public async Task Blacklist(
         BlacklistAction action,
         IRole? role = null)
@@ -111,37 +118,181 @@ public class RolePreserveModule : InteractionModuleBase
 
 
         await DeferAsync();
-        
+        switch (action)
+        {
+            case BlacklistAction.Add:
+                await BlacklistAdd(role);
+                break;
+            case BlacklistAction.Remove:
+                await BlacklistRemove(role);
+                break;
+            case BlacklistAction.List:
+                await BlacklistList(role);
+                break;
+        }
+    }
+
+    private async Task BlacklistAdd(
+        [NotNull] IRole role)
+    {
+        const string title = "Role Preserve - Add to blacklist";
         await using var db = _db.CreateSession();
         await using var trans = await db.Database.BeginTransactionAsync();
         try
         {
-
-            
-            if (action != BlacklistAction.List)
+            if (role.Guild.Id != Context.Guild.Id)
             {
-                await db.SaveChangesAsync();
-                await trans.CommitAsync();
+                await FollowupAsync(embed: new EmbedBuilder()
+                    .WithTitle(title)
+                    .WithDescription("Hey! Why are you trying to add a role to a different guild's blacklist?")
+                    .WithColor(Color.Orange)
+                    .WithCurrentTimestamp()
+                    .Build());
             }
 
-            throw new NotImplementedException();
+            var guildUser =
+                await ExceptionHelper.RetryOnTimedOut(async () => await Context.Guild.GetUserAsync(Context.User.Id))
+                ?? throw new InvalidOperationException($"Could not find requestors user in the current guild (guildId={Context.Guild.Id}, userId={Context.User.Id})");
+            var result = await _repo.RoleBlacklistAdd(db, role.Guild, role, guildUser);
+            await db.SaveChangesAsync();
+            await trans.CommitAsync();
+
+            var embed = new EmbedBuilder()
+                .WithTitle(title)
+                .WithFooter($"Role Id: {role.Id}")
+                .WithCurrentTimestamp();
+            switch (result)
+            {
+                case RolePreserveGuildRepository.RoleBlacklistAddResult.Ok:
+                    embed.WithDescription($"Removed role {role.Mention} from the blacklist.")
+                         .WithColor(Color.Green);
+                    break;
+                case RolePreserveGuildRepository.RoleBlacklistAddResult.GuildMismatch:
+                    embed.WithDescription("Hey! Why are you trying to add a role to a different guild's blacklist?")
+                         .WithColor(Color.Orange);
+                    break;
+                case RolePreserveGuildRepository.RoleBlacklistAddResult.AlreadyExists:
+                    embed.WithDescription($"Role is already blacklisted: {role.Id}")
+                         .WithColor(Color.Orange);
+                    break;
+                default:
+                    embed.WithDescription($"{result}\n-# Could not format result code")
+                        .WithColor(Color.Purple);
+                    break;
+            }
+            await FollowupAsync(embed: embed.Build());
         }
         catch (Exception ex)
         {
-            if (action != BlacklistAction.List)
-            {
-                await trans.RollbackAsync();
-            }
-            var msg = $"Failed to perform action \"{action}\" on role {role?.Name} ({role?.Id})";
+            await trans.RollbackAsync();
+            var msg = $"Failed to add role {role.Name} to blacklist (guildId={Context.Guild.Id}, roleId={role.Id})";
             _log.Error(ex, msg);
             await _error.Submit(new ErrorReportBuilder()
                 .WithException(ex)
                 .WithNotes(msg)
                 .WithContext(Context));
-            await FollowupAsync("Failed to perform action!");
+            await FollowupAsync(embed: new EmbedBuilder()
+                .WithTitle(title)
+                .WithDescription($"Failed to add role {role.Mention} to blacklist.\n-# {ex.GetType().Name}")
+                .WithColor(Color.Red)
+                .WithCurrentTimestamp()
+                .Build());
         }
+    }
 
-        throw new NotImplementedException();
+    private async Task BlacklistRemove(
+        [NotNull] IRole role)
+    {
+        const string title = "Role Preserve - Remove from blacklist";
+        await using var db = _db.CreateSession();
+        await using var trans = await db.Database.BeginTransactionAsync();
+        try
+        {
+            if (role.Guild.Id != Context.Guild.Id)
+            {
+                await FollowupAsync(embed: new EmbedBuilder()
+                    .WithTitle(title)
+                    .WithDescription("Hey! Why are you trying to remove a role from a different guild's blacklist?")
+                    .WithColor(Color.Orange)
+                    .WithCurrentTimestamp()
+                    .Build());
+            }
+
+            var result = await _repo.RoleBlacklistRemove(db, role.Guild.Id, role.Id);
+            await db.SaveChangesAsync();
+            await trans.CommitAsync();
+
+            var embed = new EmbedBuilder()
+                .WithTitle(title)
+                .WithFooter($"Role Id: {role.Id}")
+                .WithCurrentTimestamp();
+            switch (result)
+            {
+                case RolePreserveGuildRepository.RoleBlacklistRemoveResult.Ok:
+                    embed.WithDescription($"Removed role {role.Mention} from the blacklist.")
+                         .WithColor(Color.Green);
+                    break;
+                case RolePreserveGuildRepository.RoleBlacklistRemoveResult.NotFound:
+                    embed.WithDescription($"Role is not blacklisted: {role.Id}")
+                         .WithColor(Color.Orange);
+                    break;
+                default:
+                    embed.WithDescription($"{result}\n-# Could not format result code")
+                        .WithColor(Color.Purple);
+                    break;
+            }
+            await FollowupAsync(embed: embed.Build());
+        }
+        catch (Exception ex)
+        {
+            await trans.RollbackAsync();
+            var msg = $"Failed to remove role {role.Name} from blacklist (roleId={role.Id})";
+            _log.Error(ex, msg);
+            await _error.Submit(new ErrorReportBuilder()
+                .WithException(ex)
+                .WithNotes(msg)
+                .WithContext(Context));
+            await FollowupAsync(embed: new EmbedBuilder()
+                .WithTitle(title)
+                .WithDescription($"Failed to remove role {role.Mention} from blacklist.\n-# {ex.GetType().Name}")
+                .WithColor(Color.Red)
+                .WithCurrentTimestamp()
+                .Build());
+        }
+    }
+
+    private async Task BlacklistList(
+        IRole? role)
+    {
+        var embed = await ListEmbed(Context.Guild);
+        throw new NotImplementedException(
+            "List roles (basic), and implement pagination. Should only show 15 roles per page, and it should have a CSV export function. " +
+            "Data should be fetched with XeniaDiscord.Data.Repositories.RolePreserveGuildRepository.GetBlacklistRolesForGuild(XeniaDbContext, ulong)");
+    }
+
+    private async Task<EmbedBuilder> ListEmbed(
+        IGuild guild,
+        int page = 1)
+    {
+        page = int.Max(1, page);
+        const int pageSize = 10;
+        var guildIdStr = guild.Id.ToString();
+        await using var db = _db.CreateSession();
+        var skip = pageSize * (page - 1);
+        var items = await db.RolePreserveBlacklistedRoles
+            .Where(e => e.GuildId == guildIdStr)
+            .OrderByDescending(e => e.CreatedAt)
+            .Skip(skip)
+            .Take(pageSize)
+            .ToListAsync();
+        var embed = new EmbedBuilder()
+            .WithTitle("Role Preserve - Blacklisted Roles")
+            .WithDescription(string.Join("\n", items.Select(e => $"- <@&{e.RoleId}>")))
+            .WithColor(Color.Blue)
+            .WithFooter($"Page {page}")
+            .WithCurrentTimestamp();
+        // generate embed, and include pagination buttons.
+        return embed;
     }
 
     public enum BlacklistAction
