1d65e85bbf47fb4c429da1d1c73f2f244d956450
kate <kate@dariox.club>
2026-05-19 19:17:52 +0800

    XeniaBot.WebPanel: Start working on Settings controller for Role Preserve area
    Similar to what's currently in the server settings page, but it is it's own page to setup logging, enable/disable state, and blacklist.

---
+++ XeniaBot.WebPanel/Areas/RolePreserve/Controllers/SettingsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace XeniaBot.WebPanel.Areas.RolePreserve.Controllers;
+
+[Authorize]
+[AuthRequired]
+[Area("RolePreserve")]
+[Route("~/Guild/{guildId}/[area]/[controller]")]
+[RestrictToGuild(GuildIdRouteKey = "guildId")]
+public class SettingsController : Controller
+{
+    [Route("", Name = "Guild_RolePreserve_Settings_Index")]
+    public async Task<IActionResult> Index(ulong guildId)
+    {
+        // TODO implement logic to add/remove log channel, if to enable role preservation, and blacklisted roles with role preservation
+        throw new NotImplementedException();
+    }
+}
