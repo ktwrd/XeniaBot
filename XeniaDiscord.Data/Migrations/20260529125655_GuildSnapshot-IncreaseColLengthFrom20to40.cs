e86728732d0e9f5bcbe8a92b81671dae1a6ec1eb
kate <kate@dariox.club>
2026-05-29 20:57:42 +0800

    Data: Increase length of some columns (failed to import some stuff)

---
+++ XeniaDiscord.Data/Migrations/20260529125655_GuildSnapshot-IncreaseColLengthFrom20to40.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace XeniaDiscord.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class GuildSnapshotIncreaseColLengthFrom20to40 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VoiceRegionId",
+                table: "Snapshot_Guild",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VanityUrlCode",
+                table: "Snapshot_Guild",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PreferredLocale",
+                table: "Snapshot_Guild",
+                type: "character varying(40)",
+                maxLength: 40,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(20)",
+                oldMaxLength: 20);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "VoiceRegionId",
+                table: "Snapshot_Guild",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(40)",
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "VanityUrlCode",
+                table: "Snapshot_Guild",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(40)",
+                oldMaxLength: 40,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PreferredLocale",
+                table: "Snapshot_Guild",
+                type: "character varying(20)",
+                maxLength: 20,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "character varying(40)",
+                oldMaxLength: 40);
+        }
+    }
+}
