0299fdf8545b1c0018ae76fcbd89c70705fde461
kate <kate@dariox.club>
2026-08-18 18:56:47 +0800

    WebPanel: Created NotFoundApiResponse

---
+++ XeniaBot.WebPanel/Models/NotFoundApiResponse.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.Json.Serialization;
+
+namespace XeniaBot.WebPanel.Models;
+
+public class NotFoundApiResponse
+{
+    [JsonPropertyName("success")]
+    [JsonIgnore(Condition = JsonIgnoreCondition.Never)]
+    public bool Success => false;
+    
+    [JsonPropertyName("message")]
+    public required string Message { get; set; }
+}
